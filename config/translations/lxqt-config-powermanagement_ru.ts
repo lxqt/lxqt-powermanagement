dc4be63f0bc0811e7086a2c1100a2c8da3b1b292
Ilya87 <yast4ik@yahoo.com>
2014-10-19 09:15:48 +0400

    Add Russian translation

---
+++ config/translations/lxqt-config-powermanagement_ru.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Настройки наблюдения за батареей</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="37"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>&amp;Разрешить наблюдение за батареей</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="55"/>
+        <source>Warning:</source>
+        <translation>Предупреждение:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="68"/>
+        <source> seconds</source>
+        <translation> секунд</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="78"/>
+        <source>Level is low at:</source>
+        <translation>Низкий уровень при:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="91"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="98"/>
+        <source>Use icons from theme:</source>
+        <translation>Использовать значки из темы:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="112"/>
+        <source>When power is low then:</source>
+        <translation>При низком заряде:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="129"/>
+        <source>Show icon:</source>
+        <translation>Показывать значок:</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="26"/>
+        <source>Enable Idleness Watcher</source>
+        <translation>Включить наблюдение за простоем</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="47"/>
+        <source>When idle then:</source>
+        <translation>При простое:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="66"/>
+        <source>IIdle time:</source>
+        <translation>Время простоя:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="79"/>
+        <source> minutes</source>
+        <translation> минут</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="92"/>
+        <source> seconds</source>
+        <translation> секунд</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="26"/>
+        <source>Enable Lid Watcher</source>
+        <translation>Включить наблюдение за крышкой</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="45"/>
+        <source>Action when lid is closed</source>
+        <translation>Действие при закрытии крышки</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="57"/>
+        <location filename="../lidwatchersettings.ui" line="160"/>
+        <source>On Battery</source>
+        <translation>От батареи</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="77"/>
+        <source>Nothing</source>
+        <translation>Ничего</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="82"/>
+        <source>Suspend</source>
+        <translation>Ждущий режим</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="87"/>
+        <source>Hibernate</source>
+        <translation>Спящий режим</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="92"/>
+        <source>Shut down</source>
+        <translation>Выключить</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="100"/>
+        <source>On AC</source>
+        <translation>От сети</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="139"/>
+        <source>When using external monitor</source>
+        <translation>При использовании внешнего монитора</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="177"/>
+        <source>On Ac</source>
+        <translation>От сети</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <source>LXQt Powermanagement Configuration</source>
+        <translation>Настройка энергопотребления LXQt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Батарея</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Крышка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>idle</source>
+        <translation>бездействие</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Ничего</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Блокирвать экран</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Ждущий режим</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Спящий режим</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Выключить компьютер</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/lxqt-powermanagement_ru_RU.ts
@@ -0,0 +1,260 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Battery</name>
+    <message>
+        <location filename="../battery.cpp" line="136"/>
+        <source>Charging</source>
+        <translation>Заряжается</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="137"/>
+        <source>Discharging</source>
+        <translation>Разряжается</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="138"/>
+        <source>Empty</source>
+        <translation>Пустая</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="139"/>
+        <source>Fully charged</source>
+        <translation>Полностью заряжена</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="140"/>
+        <source>Pending charge</source>
+        <translation>Ожидает заряда</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="141"/>
+        <source>Pending discharge</source>
+        <translation>Ожидает разряда</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="142"/>
+        <source>Unknown</source>
+        <translation>Неизвестно</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfo</name>
+    <message>
+        <location filename="../batteryinfo.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>Управление энергопотреблением</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="25"/>
+        <source>Battery info</source>
+        <translation>Информация о батарее</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="40"/>
+        <source>Energy Now:</source>
+        <translation>Заряд сейчас:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="62"/>
+        <source>Updated:</source>
+        <translation>Обновлено:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="72"/>
+        <source>Technology:</source>
+        <translation>Технология:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="82"/>
+        <source>Model:</source>
+        <translation>Модель:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="92"/>
+        <source>Energy rate:</source>
+        <translation>Мощность тока:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="102"/>
+        <source>Voltage:</source>
+        <translation>Напряжение:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="112"/>
+        <source>Energy Full, Design:</source>
+        <translation>Максимальный заряд:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="129"/>
+        <source>Energy Full:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="181"/>
+        <source>State:</source>
+        <translation>Состояние:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="74"/>
+        <source>Lithium ion</source>
+        <translation>Литий-ионная</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="75"/>
+        <source>Lithium polymer</source>
+        <translation>Литий-полимерная</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="76"/>
+        <source>Lithium iron phosphate</source>
+        <translation>Литий-железо-фосфатная</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="77"/>
+        <source>Lead acid</source>
+        <translation>Свинцово-кислотная</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="78"/>
+        <source>Nickel cadmium</source>
+        <translation>Никель-кадмиевая</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="79"/>
+        <source>Nickel metal hydride</source>
+        <translation>Никель-металл-гидридная</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="80"/>
+        <source>Unknown</source>
+        <translation>Неизвестно</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>No battery!</source>
+        <translation>Нет батареи!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="50"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQt не смог найти информацию о какой-либо батарее - наблюдение отменено</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="91"/>
+        <source>Power low!</source>
+        <translation>Низкий заряд!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="103"/>
+        <source>Suspending in %1 seconds</source>
+        <translation>Ждущий режим через %1 секунд</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="106"/>
+        <source>Hibernating in %1 seconds</source>
+        <translation>Спящий режим через %1 секунд</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="109"/>
+        <source>Shutting down in %1 seconds</source>
+        <translation>Выключение компьютера через %1 секунд</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcher</name>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="50"/>
+        <source>LxQt Idleness watcher failed to start</source>
+        <translation>Не удалось запустить наблюдение за простоем LxQt </translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="73"/>
+        <source>D-Bus interface org.freedesktop.ScreenSaver is already registered</source>
+        <translation>Интерфейс D-Bus org.freedesktop.ScreenSaver уже зарегистрирован</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="106"/>
+        <source>The X11 Screensaver extension is not usable</source>
+        <translation>Расширение X11 Скринсейвер непригодно для использования</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="113"/>
+        <source>Configure...</source>
+        <translation>Настроить…</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="181"/>
+        <source>ERROR: Screen unlocked</source>
+        <translation>ОШИБКА: экран разблокирован</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="183"/>
+        <source>Locking program &quot;%1&quot; exited with error code %2</source>
+        <translation>Программа блокировки экрана &quot;%1&quot; вышла с кодом ошибки %2</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="185"/>
+        <source>Locking program &quot;%1&quot; crashed with error code %2</source>
+        <translation>Программа блокировки экрана &quot;%1&quot; упала с кодом ошибки %2</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="104"/>
+        <source>Power Management</source>
+        <translation>Управление энергопотреблением</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="105"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>Вы запускаете управление энергопотреблением LXQt впервые.
+Вы можете сконфигурировать его в настройках…</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="106"/>
+        <source>Configure...</source>
+        <translation>Настроить…</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>discharging</source>
+        <translation>разряжается</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>charging</source>
+        <translation>заряжается</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="133"/>
+        <location filename="../trayicon.cpp" line="167"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="132"/>
+        <source>Configure</source>
+        <translation>Настроить</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="134"/>
+        <source>Disable icon</source>
+        <translation>Не показывать значок</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="168"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Powermanagement&lt;/b&gt;&lt;br/&gt;  - Powermanagement for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;Управление энергопотреблением LXQt&lt;/b&gt;&lt;br/&gt;  - Управление энергопотреблением для окружения рабочего стола LXQt&lt;/p&gt;&lt;p&gt;  Авторы:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - и другие из проектов Razor и LXQt&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
