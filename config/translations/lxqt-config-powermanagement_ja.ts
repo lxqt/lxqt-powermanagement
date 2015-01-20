8feb58a301f3e04bc445a77df00361be32f2cdac
Mika Kobayashi <mika-k@users.noreply.github.com>
2015-01-19 21:35:44 -0500

    Add Japanese translation
    Add Japanese translated files,
    	new file:   config/translations/lxqt-config-powermanagement_ja.desktop
    	new file:   config/translations/lxqt-config-powermanagement_ja.ts
    	new file:   src/translations/lxqt-powermanagement_ja.ts

---
+++ config/translations/lxqt-config-powermanagement_ja.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>バッテリーの監視設定</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="37"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>バッテリー監視を有効にする(&amp;N)</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="55"/>
+        <source>Warning:</source>
+        <translation>警告:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="68"/>
+        <source> seconds</source>
+        <translation> 秒</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="78"/>
+        <source>Level is low at:</source>
+        <translation>低いと判断するレベル:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="91"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="98"/>
+        <source>Use icons from theme:</source>
+        <translation>テーマのアイコンを用いる:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="112"/>
+        <source>When power is low then:</source>
+        <translation>電源が低いときには:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="129"/>
+        <source>Show icon:</source>
+        <translation>アイコンを表示:</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>フォーム</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="26"/>
+        <source>Enable Idleness Watcher</source>
+        <translation>アイドル状態の監視</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="47"/>
+        <source>When idle then:</source>
+        <translation>アイドル時には:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="66"/>
+        <source>IIdle time:</source>
+        <translation>アイドル状態の時間</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="79"/>
+        <source> minutes</source>
+        <translation> 分</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="92"/>
+        <source> seconds</source>
+        <translation> 秒</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>フォーム</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="26"/>
+        <source>Enable Lid Watcher</source>
+        <translation>フタの監視を有効にする</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="45"/>
+        <source>Action when lid is closed</source>
+        <translation>フタが閉じていたときの動作</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="57"/>
+        <location filename="../lidwatchersettings.ui" line="160"/>
+        <source>On Battery</source>
+        <translation>バッテリー動作時</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="77"/>
+        <source>Nothing</source>
+        <translation>何もしない</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="82"/>
+        <source>Suspend</source>
+        <translation>サスペンド</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="87"/>
+        <source>Hibernate</source>
+        <translation>ハイバネート</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="92"/>
+        <source>Shut down</source>
+        <translation>シャットダウン</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="100"/>
+        <source>On AC</source>
+        <translation>AC電源時</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="139"/>
+        <source>When using external monitor</source>
+        <translation>外部モニター使用時</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="177"/>
+        <source>On Ac</source>
+        <translation>AC電源時</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <source>LXQt Powermanagement Configuration</source>
+        <translation>LXQt電源管理の設定</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>バッテリー</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>フタ</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>idle</source>
+        <translation>アイドル</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>何もしない</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>スクリーンをロック</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>サスペンド</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>ハイバネート</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>シャットダウン</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/lxqt-powermanagement_ja.ts
@@ -0,0 +1,259 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
+<context>
+    <name>Battery</name>
+    <message>
+        <location filename="../battery.cpp" line="136"/>
+        <source>Charging</source>
+        <translation>充電中</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="137"/>
+        <source>Discharging</source>
+        <translation>放電中</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="138"/>
+        <source>Empty</source>
+        <translation>空</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="139"/>
+        <source>Fully charged</source>
+        <translation>満充電</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="140"/>
+        <source>Pending charge</source>
+        <translation>充電中断中</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="141"/>
+        <source>Pending discharge</source>
+        <translation>放電中断中</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="142"/>
+        <source>Unknown</source>
+        <translation>不明</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfo</name>
+    <message>
+        <location filename="../batteryinfo.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>電源管理</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="25"/>
+        <source>Battery info</source>
+        <translation>バッテリー情報</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="40"/>
+        <source>Energy Now:</source>
+        <translation>現在の容量:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="62"/>
+        <source>Updated:</source>
+        <translation>更新:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="72"/>
+        <source>Technology:</source>
+        <translation>技術:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="82"/>
+        <source>Model:</source>
+        <translation>型式:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="92"/>
+        <source>Energy rate:</source>
+        <translation>エネルギー率:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="102"/>
+        <source>Voltage:</source>
+        <translation>電圧:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="112"/>
+        <source>Energy Full, Design:</source>
+        <translation>満エネルギー、デザイン:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="129"/>
+        <source>Energy Full:</source>
+        <translation>満エネルギー:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="181"/>
+        <source>State:</source>
+        <translation>状態:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="74"/>
+        <source>Lithium ion</source>
+        <translation>リチウムイオン</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="75"/>
+        <source>Lithium polymer</source>
+        <translation>リチウムポリマー</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="76"/>
+        <source>Lithium iron phosphate</source>
+        <translation>リン酸鉄リチウム</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="77"/>
+        <source>Lead acid</source>
+        <translation>鉛蓄電池</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="78"/>
+        <source>Nickel cadmium</source>
+        <translation>ニッカド</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="79"/>
+        <source>Nickel metal hydride</source>
+        <translation>ニッケル水素</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="80"/>
+        <source>Unknown</source>
+        <translation>不明</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>No battery!</source>
+        <translation>バッテリーがありません!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="50"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>バッテリーのデーターを見つけられませんので、監視機能は無効です</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="91"/>
+        <source>Power low!</source>
+        <translation>電源が少なくなっています!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="103"/>
+        <source>Suspending in %1 seconds</source>
+        <translation>%1秒でサスペンドに入ります</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="106"/>
+        <source>Hibernating in %1 seconds</source>
+        <translation>%1秒でハイバネートに入ります</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="109"/>
+        <source>Shutting down in %1 seconds</source>
+        <translation>%1秒でシャットダウンします</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcher</name>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="50"/>
+        <source>LxQt Idleness watcher failed to start</source>
+        <translation>LXQtのアイドル状態監視は始動に失敗しました</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="73"/>
+        <source>D-Bus interface org.freedesktop.ScreenSaver is already registered</source>
+        <translation>D-Busインターフェース org.freedesktop.ScreenSaver が既に登録されています</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="106"/>
+        <source>The X11 Screensaver extension is not usable</source>
+        <translation>X11スクリーンセーバー拡張を使えません</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="113"/>
+        <source>Configure...</source>
+        <translation>設定する</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="181"/>
+        <source>ERROR: Screen unlocked</source>
+        <translation>エラー: スクリーンのロックが解除されました</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="183"/>
+        <source>Locking program &quot;%1&quot; exited with error code %2</source>
+        <translation>ロック用プログラム &quot;%1&quot; は、エラーコード %2 により終了しました</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="185"/>
+        <source>Locking program &quot;%1&quot; crashed with error code %2</source>
+        <translation>ロック用プログラム &quot;%1&quot; は、エラーコード %2 によりクラッシュしました</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="104"/>
+        <source>Power Management</source>
+        <translation>電源管理</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="105"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>LXQt電源管理は初めて実行されましたので、設定を行ってください</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="106"/>
+        <source>Configure...</source>
+        <translation>設定する</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="132"/>
+        <source>Configure</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="133"/>
+        <location filename="../trayicon.cpp" line="167"/>
+        <source>About</source>
+        <translation>情報</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="134"/>
+        <source>Disable icon</source>
+        <translation>アイコンを無効</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>discharging</source>
+        <translation>放電中</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>charging</source>
+        <translation>充電中</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="168"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Powermanagement&lt;/b&gt;&lt;br/&gt;  - Powermanagement for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt Powermanagement&lt;/b&gt;&lt;br/&gt;  - LXQtデスクトップ環境の電源管理&lt;/p&gt;&lt;p&gt;  作者:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - その他 Razor-qtおよびLXQtプロジェクトのメンバー&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
