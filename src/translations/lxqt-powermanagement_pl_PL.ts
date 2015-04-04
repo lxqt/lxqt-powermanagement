53324397bf5b1a554f8938c2a648ceab4d196a10
Daniel Napora <napcok@gmail.com>
2015-04-04 11:49:49 +0200

    Polish translation updated

---
+++ src/translations/lxqt-powermanagement_pl_PL.ts
@@ -0,0 +1,260 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>Battery</name>
+    <message>
+        <location filename="../battery.cpp" line="136"/>
+        <source>Charging</source>
+        <translation>Ładowanie</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="137"/>
+        <source>Discharging</source>
+        <translation>Rozładowywanie</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="138"/>
+        <source>Empty</source>
+        <translation>Pusta</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="139"/>
+        <source>Fully charged</source>
+        <translation>W pełni naładowana</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="140"/>
+        <source>Pending charge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="141"/>
+        <source>Pending discharge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="142"/>
+        <source>Unknown</source>
+        <translation>Nieznana</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfo</name>
+    <message>
+        <location filename="../batteryinfo.ui" line="14"/>
+        <source>Power Management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="25"/>
+        <source>Battery info</source>
+        <translation>Informacje o baterii</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="40"/>
+        <source>Energy Now:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="62"/>
+        <source>Updated:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="72"/>
+        <source>Technology:</source>
+        <translation>Technologia:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="82"/>
+        <source>Model:</source>
+        <translation>Model:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="92"/>
+        <source>Energy rate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="102"/>
+        <source>Voltage:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="112"/>
+        <source>Energy Full, Design:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="129"/>
+        <source>Energy Full:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="181"/>
+        <source>State:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="74"/>
+        <source>Lithium ion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="75"/>
+        <source>Lithium polymer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="76"/>
+        <source>Lithium iron phosphate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="77"/>
+        <source>Lead acid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="78"/>
+        <source>Nickel cadmium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="79"/>
+        <source>Nickel metal hydride</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="80"/>
+        <source>Unknown</source>
+        <translation>Nieznana</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>No battery!</source>
+        <translation>Brak baterii!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="50"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQt nie znalazł danych o żadnej baterii - monitorowanie wyłączone</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="91"/>
+        <source>Power low!</source>
+        <translation>Niski poziom zasilania!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="103"/>
+        <source>Suspending in %1 seconds</source>
+        <translation>Wstrzymanie nastąpi w ciągu %1 sekund</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="106"/>
+        <source>Hibernating in %1 seconds</source>
+        <translation>Hibernacja nastąpi w ciągu %1 sekund</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="109"/>
+        <source>Shutting down in %1 seconds</source>
+        <translation>Wyłączenie systemu nastąpi w ciągu %1 sekund</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcher</name>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="50"/>
+        <source>LxQt Idleness watcher failed to start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="73"/>
+        <source>D-Bus interface org.freedesktop.ScreenSaver is already registered</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="106"/>
+        <source>The X11 Screensaver extension is not usable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="113"/>
+        <source>Configure...</source>
+        <translation>Konfiguruj...</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="181"/>
+        <source>ERROR: Screen unlocked</source>
+        <translation>BŁĄD: Ekran odblokowany</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="183"/>
+        <source>Locking program &quot;%1&quot; exited with error code %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="185"/>
+        <source>Locking program &quot;%1&quot; crashed with error code %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="104"/>
+        <source>Power Management</source>
+        <translation>Zarządzanie zasilaniem</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="105"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>Uruchamiasz zarządzanie energią LXQt po raz pierwszy.
+Możesz skonfiguroweć jego ustawienia...</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="106"/>
+        <source>Configure...</source>
+        <translation>Konfiguruj...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="132"/>
+        <source>Configure</source>
+        <translation>Konfiguruj</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="133"/>
+        <location filename="../trayicon.cpp" line="167"/>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="134"/>
+        <source>Disable icon</source>
+        <translation>Wyłącz ikonę</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>discharging</source>
+        <translation>rozładowywanie</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>charging</source>
+        <translation>ładowanie</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="168"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Powermanagement&lt;/b&gt;&lt;br/&gt;  - Powermanagement for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt Zarządzanie Energią&lt;/b&gt;&lt;br/&gt;  - Zarządzanie energią dla środowiska LXQt&lt;/p&gt;&lt;p&gt;  Autorzy:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - i inni z projektów Razor oraz LXQt&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
