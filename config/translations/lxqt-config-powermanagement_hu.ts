5b4f010ce3098438f44584677d061891eb0ef861
attus <attus@enterpriseforever.com>
2015-05-21 09:17:59 +0200

    Hungarian translations added

---
+++ config/translations/lxqt-config-powermanagement_hu.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Akkumulátor figyelő beállítás</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="37"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>Akkumulátor figyelő e&amp;ngedélyezése</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="55"/>
+        <source>Warning:</source>
+        <translation>Figyelmeztetés:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="68"/>
+        <source> seconds</source>
+        <translation> másodperc</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="78"/>
+        <source>Level is low at:</source>
+        <translation>Alacsony energiaszint:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="91"/>
+        <source> %</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="98"/>
+        <source>Use icons from theme:</source>
+        <translation>Ikonok a témából:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="112"/>
+        <source>When power is low then:</source>
+        <translation>Alacsony energiánál:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="129"/>
+        <source>Show icon:</source>
+        <translation>Ikon mutatás:</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Űrlap</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="26"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>Tétle&amp;nségi felügyelet</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="47"/>
+        <source>When idle then:</source>
+        <translation>Tétlenség esetén:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="66"/>
+        <source>Idle time:</source>
+        <translation>Tétlenségi idő:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="79"/>
+        <source> minutes</source>
+        <translation> perc</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="92"/>
+        <source> seconds</source>
+        <translation> másodperc</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Űrlap</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>Fedélfelügyelet</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="45"/>
+        <source>Action when lid is closed</source>
+        <translation>Teendő fedélzáráskor</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="57"/>
+        <location filename="../lidwatchersettings.ui" line="160"/>
+        <source>On Battery</source>
+        <translation>Akkumulátor</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="77"/>
+        <source>Nothing</source>
+        <translation>Semmi</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="82"/>
+        <source>Suspend</source>
+        <translation>Készenlét</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="87"/>
+        <source>Hibernate</source>
+        <translation>Alvás</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="92"/>
+        <source>Shut down</source>
+        <translation>Kikapcsolás</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="100"/>
+        <source>On AC</source>
+        <translation>Külső táp</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="139"/>
+        <source>When &amp;using external monitor</source>
+        <translation>&amp;Külső monitor használatakor</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="177"/>
+        <source>On Ac</source>
+        <translation>Külső táp</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <source>LXQt Powermanagement Configuration</source>
+        <translation>LXQt energiafelügyelet beállítás</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Akkumulátor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Fedél</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>idle</source>
+        <translation>Tétlenség</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Semmi</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Képernyőzár</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Készenlét</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Alvás</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Kikapcsolás</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/lxqt-powermanagement_hu.ts
@@ -0,0 +1,260 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hu_HU">
+<context>
+    <name>Battery</name>
+    <message>
+        <location filename="../battery.cpp" line="136"/>
+        <source>Charging</source>
+        <translation>Töltés</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="137"/>
+        <source>Discharging</source>
+        <translation>Kisütés</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="138"/>
+        <source>Empty</source>
+        <translation>Üres</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="139"/>
+        <source>Fully charged</source>
+        <translation>Teletöltve</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="140"/>
+        <source>Pending charge</source>
+        <translation>Tölthetőség</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="141"/>
+        <source>Pending discharge</source>
+        <translation>Kisüthetőség</translation>
+    </message>
+    <message>
+        <location filename="../battery.cpp" line="142"/>
+        <source>Unknown</source>
+        <translation>Ismeretlen</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfo</name>
+    <message>
+        <location filename="../batteryinfo.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>Energiakezelés</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="25"/>
+        <source>Battery info</source>
+        <translation>Akkumulátor információ</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="40"/>
+        <source>Energy Now:</source>
+        <translation>Állapot:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="62"/>
+        <source>Updated:</source>
+        <translation>Frissítve:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="72"/>
+        <source>Technology:</source>
+        <translation>Technológia:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="82"/>
+        <source>Model:</source>
+        <translation>Modell:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="92"/>
+        <source>Energy rate:</source>
+        <translation>Energiaállapot:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="102"/>
+        <source>Voltage:</source>
+        <translation>Feszültség:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="112"/>
+        <source>Energy Full, Design:</source>
+        <translation>Névleges kapacitás: </translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="129"/>
+        <source>Energy Full:</source>
+        <translation>Teljes kapacitás:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.ui" line="181"/>
+        <source>State:</source>
+        <translation>Állapot:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="74"/>
+        <source>Lithium ion</source>
+        <translation>Lítium ion</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="75"/>
+        <source>Lithium polymer</source>
+        <translation>Lítium polimer</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="76"/>
+        <source>Lithium iron phosphate</source>
+        <translation>Lítium vasfoszfát</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="77"/>
+        <source>Lead acid</source>
+        <translation>Savas ólom</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="78"/>
+        <source>Nickel cadmium</source>
+        <translation>Nikkel kadmium</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="79"/>
+        <source>Nickel metal hydride</source>
+        <translation>Nikkel fémhidrid</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfo.cpp" line="80"/>
+        <source>Unknown</source>
+        <translation>Ismeretlen</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>No battery!</source>
+        <translation>Nincs akku!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="50"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>Az LXQt nem ismert fel akkut - figyelés tiltva</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="91"/>
+        <source>Power low!</source>
+        <translation>Alacsony feszültség!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="103"/>
+        <source>Suspending in %1 seconds</source>
+        <translation>%1 másodperc múlva készülség</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="106"/>
+        <source>Hibernating in %1 seconds</source>
+        <translation>%1 másodperc múlva mélykészenlét</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="109"/>
+        <source>Shutting down in %1 seconds</source>
+        <translation>%1 másodperc múlva kikapcsolás</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcher</name>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="50"/>
+        <source>LxQt Idleness watcher failed to start</source>
+        <translation>Az LXQt tétlenség figyelő indítása sikertelen</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="73"/>
+        <source>D-Bus interface org.freedesktop.ScreenSaver is already registered</source>
+        <translation>A D-Bus interface org.freedesktop.ScreenSaver imár regisztrálva van.</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="106"/>
+        <source>The X11 Screensaver extension is not usable</source>
+        <translation>Az X11 képernyővédő bővítmény használhatatlan</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="113"/>
+        <source>Configure...</source>
+        <translation>Beállítás...</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="181"/>
+        <source>ERROR: Screen unlocked</source>
+        <translation>HIBA: A képernyő nem zárolt</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="183"/>
+        <source>Locking program &quot;%1&quot; exited with error code %2</source>
+        <translation>A&quot;%1&quot; program %2 hibakóddal kilépett</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatcher.cpp" line="185"/>
+        <source>Locking program &quot;%1&quot; crashed with error code %2</source>
+        <translation>A&quot;%1&quot; program %2 hibakóddal összeomlott</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="104"/>
+        <source>Power Management</source>
+        <translation>Energiakezelés</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="105"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>Az LXQt energiakezelő első alkalommal fut.
+A beállításokban konfigurálható...</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="106"/>
+        <source>Configure...</source>
+        <translation>Beállítás...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="132"/>
+        <source>Configure</source>
+        <translation>Beállítás</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="133"/>
+        <location filename="../trayicon.cpp" line="167"/>
+        <source>About</source>
+        <translation>Rólunk</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="134"/>
+        <source>Disable icon</source>
+        <translation>Ikon tiltása</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>discharging</source>
+        <translation>kisütés</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="154"/>
+        <source>charging</source>
+        <translation>töltés</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="168"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Powermanagement&lt;/b&gt;&lt;br/&gt;  - Powermanagement for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt energiakezelő&lt;/b&gt;&lt;br/&gt;  - Energiakezelő az LXQt asztal részére&lt;/p&gt;&lt;p&gt;  Szerzők:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - és még többen a Razor és LXQt fejlesztőiből.&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
