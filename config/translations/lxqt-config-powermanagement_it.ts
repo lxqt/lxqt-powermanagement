7ee2cb0208d1effed6aa0df67b481d6fb1efeb88
stefonarch <stef.binde@riseup.net>
2015-05-26 10:09:44 +0200

    Added italian translations

---
+++ config/translations/lxqt-config-powermanagement_it.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it" sourcelanguage="it">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Impostazioni monitor batteria</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="37"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>&amp;Attiva il monitor della batteria</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="55"/>
+        <source>Warning:</source>
+        <translation>Tempo di attesa:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="68"/>
+        <source> seconds</source>
+        <translation> secondi</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="78"/>
+        <source>Level is low at:</source>
+        <translation>Livello basso :</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="91"/>
+        <source> %</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="98"/>
+        <source>Use icons from theme:</source>
+        <translation>Usa icone del tema:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="112"/>
+        <source>When power is low then:</source>
+        <translation>Quando il livello è basso:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="129"/>
+        <source>Show icon:</source>
+        <translation>Mostra icona:</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="26"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>&amp;Attiva monitor attività sistema</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="47"/>
+        <source>When idle then:</source>
+        <translation>Quando inattivo:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="66"/>
+        <source>Idle time:</source>
+        <translation>Tempo di inattività:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="79"/>
+        <source> minutes</source>
+        <translation> minuti</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="92"/>
+        <source> seconds</source>
+        <translation> secondi</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>&amp;Attiva monitor coperchio</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="45"/>
+        <source>Action when lid is closed</source>
+        <translation>Azione a chiusura</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="57"/>
+        <location filename="../lidwatchersettings.ui" line="160"/>
+        <source>On Battery</source>
+        <translation>Alimentazione a batteria</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="77"/>
+        <source>Nothing</source>
+        <translation>Niente</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="82"/>
+        <source>Suspend</source>
+        <translation>Sospendi</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="87"/>
+        <source>Hibernate</source>
+        <translation>Iberna</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="92"/>
+        <source>Shut down</source>
+        <translation>Spegni</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="100"/>
+        <source>On AC</source>
+        <translation>Alimentazione di rete</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="139"/>
+        <source>When &amp;using external monitor</source>
+        <translation>Quando è in uso un monitor &amp;esterno</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="177"/>
+        <source>On Ac</source>
+        <translation>Alimentazione di rete</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <source>LXQt Powermanagement Configuration</source>
+        <translation>Configurazione gestione energetica di LXQt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Batteria</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Coperchio</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>idle</source>
+        <translation>Inattività</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Nessuna azione</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Blocca schermo</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Sospendi</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Iberna</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Spegni</translation>
+    </message>
+</context>
+</TS>
