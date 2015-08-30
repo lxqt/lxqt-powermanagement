8fcba0e6cd37a597a2304cf1290b59a80df5f284
Dimitrios Glentadakis <dglent@free.fr>
2015-08-30 08:30:58 +0200

    Add Greek (el) translation Add the 'Comment' key in desktop file

---
+++ config/translations/lxqt-config-powermanagement_el.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Ρυθμίσεις επόπτη μπαταρίας</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="37"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>&amp;Ενεργοποίηση του επόπτη της μπαταρίας</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="55"/>
+        <source>Warning:</source>
+        <translation>Προειδοποίηση:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="68"/>
+        <source> seconds</source>
+        <translation> δευτερόλεπτα</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="78"/>
+        <source>Level is low at:</source>
+        <translation>Το επίπεδο είναι χαμηλό στο:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="91"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="98"/>
+        <source>Use icons from theme:</source>
+        <translation>Χρήση των εικονιδίων από το θέμα:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="112"/>
+        <source>When power is low then:</source>
+        <translation>Όταν η ενέργεια είναι χαμηλά τότε:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="129"/>
+        <source>Show icon:</source>
+        <translation>Εμφάνιση εικονιδίου:</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Φόρμα</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="26"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>Ενεργοποίηση του επόπτη α&amp;δράνειας</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="47"/>
+        <source>When idle then:</source>
+        <translation>Όταν είναι αδρανής:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="66"/>
+        <source>Idle time:</source>
+        <translation>Χρόνος αδράνειας:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="79"/>
+        <source> minutes</source>
+        <translation> λεπτά</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="92"/>
+        <source> seconds</source>
+        <translation> δευτερόλεπτα</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Φόρμα</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>Ενεργοποίηση του επόπτη &amp;καλύμματος</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="45"/>
+        <source>Action when lid is closed</source>
+        <translation>Ενέργεια κατά το κλείσιμο του καλύμματος</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="57"/>
+        <location filename="../lidwatchersettings.ui" line="160"/>
+        <source>On Battery</source>
+        <translation>Σε μπαταρία</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="77"/>
+        <source>Nothing</source>
+        <translation>Τίποτα</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="82"/>
+        <source>Suspend</source>
+        <translation>Αναστολή</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="87"/>
+        <source>Hibernate</source>
+        <translation>Νάρκη</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="92"/>
+        <source>Shut down</source>
+        <translation>Τερματισμός</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="100"/>
+        <source>On AC</source>
+        <translation>Σε τροφοδοτικό AC</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="139"/>
+        <source>When &amp;using external monitor</source>
+        <translation>Όταν χρησιμοποιείται εξωτερική ο&amp;θόνη</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="177"/>
+        <source>On Ac</source>
+        <translation>Σε τροφοδοτικό AC</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <source>LXQt Powermanagement Configuration</source>
+        <translation>Διαμόρφωση της διαχείρισης ενέργειας του LXQt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Μπαταρία</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Κάλυμμα φορητού</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>idle</source>
+        <translation>Αδράνεια</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Τίποτα</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Κλείδωμα οθόνης</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Αναστολή</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Νάρκη</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Τερματισμός</translation>
+    </message>
+</context>
+</TS>
