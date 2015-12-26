5050da67d4b44b74e7624095c7d5df36e2a7caee
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2015-12-26 22:03:28 +0100

    Fix source language in Italian translation file
    The file's source language was set to "it".

--- config/translations/lxqt-config-powermanagement_it.ts
+++ config/translations/lxqt-config-powermanagement_it.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="it" sourcelanguage="it">
+<TS version="2.1" language="it">
 <context>
     <name>BatteryWatcherSettings</name>
     <message>
