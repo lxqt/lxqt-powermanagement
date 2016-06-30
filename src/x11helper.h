bc3e49a6ed344118a88363039e0c37f93f96561a
Luís Pereira <luis.artur.pereira@gmail.com>
2016-06-30 12:58:42 +0100

    Don't lock the screen when it's already locked.
    The status of the screensaver wasn't taken into account when locking it.
    Now we only lock it if it's not already locked. This way we don't get the
    annoying error message stating that there was an error starting the
    screensaver.
    
    Part of the code is taken from the Chromium project. Adapted to fit our
    purposes.

--- src/x11helper.h
+++ src/x11helper.h
@@ -5,6 +5,8 @@
  * http://razor-qt.org
  *
  * Copyright (C) 2013  Alec Moskvin <alecm@gmx.com>
+ * Copyright (c) 2016 Luís Pereira <luis.artur.pereira@gmail.com>
+ * Copyright (c) 2013 The Chromium Authors. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,10 +27,27 @@
 #ifndef X11HELPER_H
 #define X11HELPER_H
 
+#include <memory>
+
 #include <xcb/xcb.h>
 
 // Avoid polluting everything with X11/Xlib.h:
 typedef struct _XDisplay Display;
+
+typedef unsigned long XAtom;
+typedef unsigned long XID;
+
+extern "C" {
+int XFree(void*);
+}
+
+template <class T, class R, R (*F)(T*)>
+struct XObjectDeleter {
+  inline void operator()(void* ptr) const { F(static_cast<T*>(ptr)); }
+};
+
+template <class T, class D = XObjectDeleter<void, int, XFree>>
+using XScopedPtr = std::unique_ptr<T, D>;
 
 /**
  * @brief The X11Helper class is class to get the X11 Display or XCB connection
@@ -49,6 +68,8 @@ public:
      * @return
      */
     static xcb_connection_t* connection();
+
+    static bool isScreenSaverLocked();
 };
 
 #endif // X11HELPER_H
