4fc3747814e97b4059d4b13ab0d2133a7f5ae1c6
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-12 08:33:53 +0200

    lxqt-powermanagement: Add Catalan translations
        Add lxqt-powermanagement_ca.ts (Catalan)
        Add lxqt-config-powermanagement_ca.ts (Catalan)

---
+++ lxqt-powermanagement_ca.ts
@@ -0,0 +1,313 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>BatteryHelper</name>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="42"/>
+        <source>Empty</source>
+        <translation>Buida</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="44"/>
+        <source>Discharging</source>
+        <translation>S&apos;està descarregant</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="46"/>
+        <source>Fully charged</source>
+        <translation>Completament carregada</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="49"/>
+        <source>Charging</source>
+        <translation>S&apos;està carregant</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="58"/>
+        <source>Lithium ion</source>
+        <translation>Ió liti</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="60"/>
+        <source>Lithium polymer</source>
+        <translation>Ió liti en polímer</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="62"/>
+        <source>Lithium iron phosphate</source>
+        <translation>Fosfat de ferro i liti</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="64"/>
+        <source>Lead acid</source>
+        <translation>Plom i àcid</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="66"/>
+        <source>Nickel cadmium</source>
+        <translation>Níquel i cadmi</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="68"/>
+        <source>Nickel metal hydride</source>
+        <translation>Hidrur metàl·lic de níquel</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="71"/>
+        <source>Unknown</source>
+        <translation>Desconeguda</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="80"/>
+        <source>Personal Digital Assistant&apos;s battery</source>
+        <translation>Bateria d&apos;ordinador de butxaca</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="82"/>
+        <source>Uninterruptible Power Supply&apos;s battery</source>
+        <translation>Bateria de sistema d&apos;alimentació ininterrompuda</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="84"/>
+        <source>Primary battery</source>
+        <translation>Bateria primària</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="86"/>
+        <source>Mouse battery</source>
+        <translation>Bateria de ratolí</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="88"/>
+        <source>Keyboard battery</source>
+        <translation>Bateria de teclat</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="90"/>
+        <source>Keyboard and mouse&apos;s battery</source>
+        <translation>Bateria de teclat i ratolí</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="92"/>
+        <source>Camera battery</source>
+        <translation>Bateria de càmera</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="94"/>
+        <source>Phone battery</source>
+        <translation>Bateria de telèfon</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="96"/>
+        <source>Monitor battery</source>
+        <translation>Bateria de monitor</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="99"/>
+        <source>Unknown battery</source>
+        <translation>Bateria desconeguda</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoDialog</name>
+    <message>
+        <location filename="../../../batteryinfodialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Diàleg</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfodialog.cpp" line="22"/>
+        <source>Battery Info</source>
+        <translation>Informació de la bateria</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoFrame</name>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>Gestió de l&apos;energia</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="53"/>
+        <source>Temperature:</source>
+        <translation>Temperatura:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="63"/>
+        <source>Type:</source>
+        <translation>Tipus:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="73"/>
+        <source>State:</source>
+        <translation>Estat:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="90"/>
+        <source>Energy Full Design:</source>
+        <translation>Energia disseny-producció:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="107"/>
+        <source>Energy Full:</source>
+        <translation>Energia plena:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="124"/>
+        <source>Energy Now:</source>
+        <translation>Energia ara:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="141"/>
+        <source>Energy rate:</source>
+        <translation>Cadència d&apos;energia:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="165"/>
+        <source>Technology:</source>
+        <translation>Tecnologia:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="182"/>
+        <source>Voltage:</source>
+        <translation>Voltatge:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="192"/>
+        <source>Vendor:</source>
+        <translation>Fabricant:</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="47"/>
+        <source>No battery!</source>
+        <translation>Sense bateria!</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="48"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQt no ha pogut trobar les dades de cap bateria - s&apos;ha inhabilitat el monitoratge</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="113"/>
+        <source>Power low!</source>
+        <translation>Energia baixa!</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="125"/>
+        <source>Suspending in %1 seconds</source>
+        <translation>Se suspèn en %1 segons</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="128"/>
+        <source>Hibernating in %1 seconds</source>
+        <translation>S&apos;hiberna en %1 segons</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="131"/>
+        <source>Shutting down in %1 seconds</source>
+        <translation>S&apos;apaga en %1 segons</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcher</name>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="50"/>
+        <source>LXQt Idleness watcher failed to start</source>
+        <translation>L&apos;observador de l&apos;ociositat de LXQt ha fallat en iniciar-se</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="73"/>
+        <source>D-Bus interface org.freedesktop.ScreenSaver is already registered</source>
+        <translation>La interfície de D-Bus org.freedesktop.ScreenSaver ja està registrada</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="107"/>
+        <source>The X11 Screensaver extension is not usable</source>
+        <translation>L&apos;extensió de l&apos;estalvi de pantalla de X11 no és usable</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="114"/>
+        <source>Configure...</source>
+        <translation>Configura...</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="179"/>
+        <source>ERROR: Screen unlocked</source>
+        <translation>ERROR: Pantalla desbloquejada</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="181"/>
+        <source>Locking program &quot;%1&quot; exited with error code %2</source>
+        <translation>S&apos;està bloquejant el programa &quot;%1&quot; que va sortir amb el codi d&apos;error %2</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="183"/>
+        <source>Locking program &quot;%1&quot; crashed with error code %2</source>
+        <translation>S&apos;està bloquejant el programa &quot;%1&quot; que va estavellar-se amb el codi d&apos;error %2</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../../../powermanagementd.cpp" line="107"/>
+        <source>Power Management</source>
+        <translation>Gestió de l&apos;energia</translation>
+    </message>
+    <message>
+        <location filename="../../../powermanagementd.cpp" line="108"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>Esteu executant per primera vegada la gestió de l&apos;energia de LXQt.
+Podeu configurar-ho a partir dels ajusts... </translation>
+    </message>
+    <message>
+        <location filename="../../../powermanagementd.cpp" line="109"/>
+        <source>Configure...</source>
+        <translation>Configura...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../../../trayicon.cpp" line="59"/>
+        <source>Configure</source>
+        <translation>Configura</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="61"/>
+        <location filename="../../../trayicon.cpp" line="92"/>
+        <source>About</source>
+        <translation>Quant a</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="63"/>
+        <source>Disable icon</source>
+        <translation>Inhabilita la icona</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="93"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;Gestió de l&apos;energia de LXQt&lt;/b&gt;&lt;br/&gt;  - Gestió de l&apos;energia per a l&apos;entorn d&apos;escriptori LXQt&lt;/p&gt;&lt;p&gt;  Autors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - i altres des dels projectes Razor i LXQt&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="111"/>
+        <source>LXQt Power Management info</source>
+        <translation>Informació de la gestió de l&apos;energia de LXQt</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="112"/>
+        <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
+        <translation>La icona de la safata de la gestió de l&apos;energia de LXQt pot ser (re)habilitada a &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="114"/>
+        <source>Configure now</source>
+        <translation>Configura ara</translation>
+    </message>
+</context>
+</TS>
