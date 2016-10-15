c0a7d4291e97ec8d425d5bf54e8cd3aafd301ef1
Pjotr123 <pjotrvertaalt@gmail.com>
2016-10-15 23:24:21 +0200

    Create lxqt-config-powermanagement_nl.ts
    Complete Dutch translation.
    
    Please excuse me for not combining pull requests. I don't know how....

---
+++ lxqt-config-powermanagement_nl.ts
@@ -0,0 +1,217 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Instellingen voor Accubewaker</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="26"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>Accubewaker &amp;inschakelen</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="54"/>
+        <source>Warning:</source>
+        <translation>Waarschuwing:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="67"/>
+        <source> seconds</source>
+        <translation> seconden</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="77"/>
+        <source>Level is low at:</source>
+        <translation>Niveau is laag bij:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="90"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="111"/>
+        <source>Use icons from theme:</source>
+        <translation>gebruik pictogrammen van thema:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="125"/>
+        <location filename="../../../batterywatchersettings.cpp" line="93"/>
+        <source>Preview</source>
+        <translation>Voorbeeldweergave</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="162"/>
+        <source>Charging</source>
+        <translation>Laden</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="200"/>
+        <source>Discharging</source>
+        <translation>Ontladen</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="41"/>
+        <source>When power is low then:</source>
+        <translation>Bij lage lading:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="97"/>
+        <source>Show icon:</source>
+        <translation>Toon pictogram:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.cpp" line="93"/>
+        <source>built in</source>
+        <translation>ingebouwd</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.cpp" line="98"/>
+        <source>Level: %1%</source>
+        <translation>Lading: %1 %</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Vorm</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="20"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>&amp;Inactiviteitsbewaking inschakelen</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="41"/>
+        <source>When idle then:</source>
+        <translation>Bij inactiviteit:</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="60"/>
+        <source>Idle time:</source>
+        <translation>Duur van inactiviteit:</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="73"/>
+        <source> minutes</source>
+        <translation> minuten</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="86"/>
+        <source> seconds</source>
+        <translation> seconden</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Vorm</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>&amp;Dekselbewaking inschakelen</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="41"/>
+        <source>Action when lid is closed</source>
+        <translation>Actie wanneer deksel wordt gesloten</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="53"/>
+        <location filename="../../../lidwatchersettings.ui" line="118"/>
+        <source>On Battery</source>
+        <translation>Op accu</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="61"/>
+        <source>Nothing</source>
+        <translation>Niets</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="66"/>
+        <source>Suspend</source>
+        <translation>Pauzestand</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="71"/>
+        <source>Hibernate</source>
+        <translation>Slaapstand</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="76"/>
+        <source>Shut down</source>
+        <translation>Afsluiten</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="84"/>
+        <location filename="../../../lidwatchersettings.ui" line="128"/>
+        <source>On AC</source>
+        <translation>Op netstroom</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="97"/>
+        <source>When &amp;using external monitor</source>
+        <translation>Bij gebr&amp;uik van een extern beeldscherm</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="34"/>
+        <source>Power Management Settings</source>
+        <translation>Instellingen van Energiebeheer</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Accu</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Deksel</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="45"/>
+        <source>Idle</source>
+        <translation>Inactief</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Niets</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Scherm vergrendelen</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Pauzestand</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Slaapstand</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Afsluiten</translation>
+    </message>
+</context>
+</TS>
