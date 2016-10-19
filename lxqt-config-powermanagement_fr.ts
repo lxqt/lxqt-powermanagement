921e310607160bb014f58e398fadf77870e482af
paiiou <paiiou@free.fr>
2016-10-19 10:18:38 +0200

    Update French translations
    liblxqt, about, notificationd, openssh-askpass, policykit,
    powermanagement, runner, session, sudo

---
+++ lxqt-config-powermanagement_fr.ts
@@ -0,0 +1,217 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Paramétrage du contrôle des batteries</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="26"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>&amp;Valider le contrôle des datteries</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="41"/>
+        <source>When power is low then:</source>
+        <translation>Que faire si la batterie est faible :</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="54"/>
+        <source>Warning:</source>
+        <translation>Attention :</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="67"/>
+        <source> seconds</source>
+        <translation> s</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="77"/>
+        <source>Level is low at:</source>
+        <translation>Le niveau est faible pour :</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="90"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="97"/>
+        <source>Show icon:</source>
+        <translation>Afficher l&apos;icône :</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="111"/>
+        <source>Use icons from theme:</source>
+        <translation>Utiliser les icônes du thème :</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="125"/>
+        <location filename="../batterywatchersettings.cpp" line="93"/>
+        <source>Preview</source>
+        <translation>Aperçu</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="162"/>
+        <source>Charging</source>
+        <translation>En cours de charge</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="200"/>
+        <source>Discharging</source>
+        <translation>Décharge</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="93"/>
+        <source>built in</source>
+        <translation>intégré(e)</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="98"/>
+        <source>Level: %1%</source>
+        <translation>Niveau %1%</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulaire</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="20"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>Valider le contrôle de &amp;veille</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="41"/>
+        <source>When idle then:</source>
+        <translation>En veille, alors :</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="60"/>
+        <source>Idle time:</source>
+        <translation>Durée de veille :</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="73"/>
+        <source> minutes</source>
+        <translation> mn</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="86"/>
+        <source> seconds</source>
+        <translation> s</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulaire</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>Valider le &amp;contrôle du capot</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="41"/>
+        <source>Action when lid is closed</source>
+        <translation>Action lorsque le capot est fermé</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="53"/>
+        <location filename="../lidwatchersettings.ui" line="118"/>
+        <source>On Battery</source>
+        <translation>Sur batterie</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="61"/>
+        <source>Nothing</source>
+        <translation>Rien</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="66"/>
+        <source>Suspend</source>
+        <translation>Suspendre</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="71"/>
+        <source>Hibernate</source>
+        <translation>Hiberner</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="76"/>
+        <source>Shut down</source>
+        <translation>Arrêter</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="84"/>
+        <location filename="../lidwatchersettings.ui" line="128"/>
+        <source>On AC</source>
+        <translation>Sur prise de courant</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="97"/>
+        <source>When &amp;using external monitor</source>
+        <translation>&amp;Utilisation d&apos;un moniteur externe</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <source>Power Management Settings</source>
+        <translation>Paramétrage de la gestion de l&apos;énergie</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Batterie</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Capot</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>Idle</source>
+        <translation>Veille</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Aucun</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Verrouiller l&apos;écran</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Suspension</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Hibernation</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Arrêt</translation>
+    </message>
+</context>
+</TS>
