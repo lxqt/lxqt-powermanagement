36b4038f2a8d9817ce4d20afc0f6c99a57b08091
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-12-21 00:07:49 +0200

    Add/update Turkish translations

---
+++ lxqt-config-powermanagement_tr.ts
@@ -0,0 +1,217 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Pil İzleyici Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="26"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>Pil İzleyici &amp;Etkin</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="54"/>
+        <source>Warning:</source>
+        <translation>Uyarı:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="67"/>
+        <source> seconds</source>
+        <translation>saniye</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="77"/>
+        <source>Level is low at:</source>
+        <translation>Kritik seviye:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="90"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="111"/>
+        <source>Use icons from theme:</source>
+        <translation>Tema simgesini kullan:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="125"/>
+        <location filename="../../../batterywatchersettings.cpp" line="93"/>
+        <source>Preview</source>
+        <translation>Önizle</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="162"/>
+        <source>Charging</source>
+        <translation>Doluyor</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="200"/>
+        <source>Discharging</source>
+        <translation>Boşalıyor</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="41"/>
+        <source>When power is low then:</source>
+        <translation type="unfinished">Pil durumu kritik seviyedeyken:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="97"/>
+        <source>Show icon:</source>
+        <translation>Simgeyi göster:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.cpp" line="93"/>
+        <source>built in</source>
+        <translation>sistem</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.cpp" line="98"/>
+        <source>Level: %1%</source>
+        <translation>Seviye: %1%</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="20"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>Bekleme İzeyicisini Etkin&amp;leştir</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="41"/>
+        <source>When idle then:</source>
+        <translation>Boştayken:</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="60"/>
+        <source>Idle time:</source>
+        <translation>Boşta bekleme süresi:</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="73"/>
+        <source> minutes</source>
+        <translation>dakika</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="86"/>
+        <source> seconds</source>
+        <translation>saniye</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>&amp;Kapak İzleyicisini Etkinleştir</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="41"/>
+        <source>Action when lid is closed</source>
+        <translation>Kapak kapatıldığında</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="53"/>
+        <location filename="../../../lidwatchersettings.ui" line="118"/>
+        <source>On Battery</source>
+        <translation>Pil durumunda</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="61"/>
+        <source>Nothing</source>
+        <translation>Hiçbirşey</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="66"/>
+        <source>Suspend</source>
+        <translation>Beklemeye Al</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="71"/>
+        <source>Hibernate</source>
+        <translation>Uykuya Al</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="76"/>
+        <source>Shut down</source>
+        <translation>Kapat</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="84"/>
+        <location filename="../../../lidwatchersettings.ui" line="128"/>
+        <source>On AC</source>
+        <translation>Prize Takılı Durumda</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="97"/>
+        <source>When &amp;using external monitor</source>
+        <translation>Harici ekran k&amp;ullanırken</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="34"/>
+        <source>Power Management Settings</source>
+        <translation>Güç Yönetim Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Pil</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Kapak</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="45"/>
+        <source>Idle</source>
+        <translation>Bekleme</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Hiçbirşey</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Ekranı kilitle</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Beklemeye Al</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Uykuya Al</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Kapat</translation>
+    </message>
+</context>
+</TS>
