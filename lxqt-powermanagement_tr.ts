e8555caa52be71b0e5065e426c3856514ed41e0a
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-12-21 00:07:49 +0200

    Add/update Turkish translations

---
+++ lxqt-powermanagement_tr.ts
@@ -0,0 +1,313 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>BatteryHelper</name>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="42"/>
+        <source>Empty</source>
+        <translation>Boş</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="44"/>
+        <source>Discharging</source>
+        <translation>Boşalıyor</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="46"/>
+        <source>Fully charged</source>
+        <translation>Tamamen doldu</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="49"/>
+        <source>Charging</source>
+        <translation>Doluyor</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="58"/>
+        <source>Lithium ion</source>
+        <translation>Lityum İyon</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="60"/>
+        <source>Lithium polymer</source>
+        <translation>Lityum polimer</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="62"/>
+        <source>Lithium iron phosphate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="64"/>
+        <source>Lead acid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="66"/>
+        <source>Nickel cadmium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="68"/>
+        <source>Nickel metal hydride</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="71"/>
+        <source>Unknown</source>
+        <translation>Bilinmeyen</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="80"/>
+        <source>Personal Digital Assistant&apos;s battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="82"/>
+        <source>Uninterruptible Power Supply&apos;s battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="84"/>
+        <source>Primary battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="86"/>
+        <source>Mouse battery</source>
+        <translation>Fare pili</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="88"/>
+        <source>Keyboard battery</source>
+        <translation>Klavye pili</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="90"/>
+        <source>Keyboard and mouse&apos;s battery</source>
+        <translation>Klavye ve fare pili</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="92"/>
+        <source>Camera battery</source>
+        <translation>Kamera pili</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="94"/>
+        <source>Phone battery</source>
+        <translation>Telefon pili</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="96"/>
+        <source>Monitor battery</source>
+        <translation>Ekran pili</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryhelper.cpp" line="99"/>
+        <source>Unknown battery</source>
+        <translation>Bilinmeyen pil</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoDialog</name>
+    <message>
+        <location filename="../../../batteryinfodialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Pencere</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfodialog.cpp" line="22"/>
+        <source>Battery Info</source>
+        <translation>Pil Bilgisi</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoFrame</name>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>Güç Yönetimi</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="53"/>
+        <source>Temperature:</source>
+        <translation>Isı:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="63"/>
+        <source>Type:</source>
+        <translation>Tür:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="73"/>
+        <source>State:</source>
+        <translation>Durum:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="90"/>
+        <source>Energy Full Design:</source>
+        <translation>Enerji Tam Durumu:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="107"/>
+        <source>Energy Full:</source>
+        <translation>Tam Enerji:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="124"/>
+        <source>Energy Now:</source>
+        <translation>Şimdiki Eneji:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="141"/>
+        <source>Energy rate:</source>
+        <translation>Enerji oranı:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="165"/>
+        <source>Technology:</source>
+        <translation>Teknoloji:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="182"/>
+        <source>Voltage:</source>
+        <translation>Voltaj:</translation>
+    </message>
+    <message>
+        <location filename="../../../batteryinfoframe.ui" line="192"/>
+        <source>Vendor:</source>
+        <translation>Üretici:</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="47"/>
+        <source>No battery!</source>
+        <translation>Pil yok!</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="48"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQT bir batarya verisi bulmadı - izleme iptal edildi</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="113"/>
+        <source>Power low!</source>
+        <translation>Pil Zayıf!</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="125"/>
+        <source>Suspending in %1 seconds</source>
+        <translation>Bilgisayar %1 saniye içinde beklemeye alınacak</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="128"/>
+        <source>Hibernating in %1 seconds</source>
+        <translation>Bilgisayar %1 saniye içinde uykuya alınacak</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatcher.cpp" line="131"/>
+        <source>Shutting down in %1 seconds</source>
+        <translation>Bilgisayar %1 saniye içinde kapatlacak</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcher</name>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="50"/>
+        <source>LXQt Idleness watcher failed to start</source>
+        <translation>LXQT Bekleme izleyicisi başlatılamadı</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="73"/>
+        <source>D-Bus interface org.freedesktop.ScreenSaver is already registered</source>
+        <translation>D-Bus arayüzü org.freedesktop.ScreenSaver zaten kayıtlı</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="107"/>
+        <source>The X11 Screensaver extension is not usable</source>
+        <translation>X11 Screensaver eklentisi kullanılamaz</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="114"/>
+        <source>Configure...</source>
+        <translation>Yapılandır...</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="179"/>
+        <source>ERROR: Screen unlocked</source>
+        <translation>HATA: Ekran kilidi açılamadı</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="181"/>
+        <source>Locking program &quot;%1&quot; exited with error code %2</source>
+        <translation>Kilitleme programı &quot;%1&quot; hata kodu %2 ile çıktı</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatcher.cpp" line="183"/>
+        <source>Locking program &quot;%1&quot; crashed with error code %2</source>
+        <translation>Kilitleme programı &quot;%1&quot; hata kodu %2 ile çöktü</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../../../powermanagementd.cpp" line="107"/>
+        <source>Power Management</source>
+        <translation>Güç Yönetimi</translation>
+    </message>
+    <message>
+        <location filename="../../../powermanagementd.cpp" line="108"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>LXQT Güç Yönetimini ilk defa çalıştırıyorsunuz. 
+Güç yönetimini ayarladan yapılandırabilirsiniz...</translation>
+    </message>
+    <message>
+        <location filename="../../../powermanagementd.cpp" line="109"/>
+        <source>Configure...</source>
+        <translation>Yapılandır...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../../../trayicon.cpp" line="59"/>
+        <source>Configure</source>
+        <translation>Yapılandır</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="61"/>
+        <location filename="../../../trayicon.cpp" line="92"/>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="63"/>
+        <source>Disable icon</source>
+        <translation>Simgeyi kapat</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="93"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt Güç Yönetimi&lt;/b&gt;&lt;br/&gt;  -LXQt Masaüstü Ortamı için Güç Yöneticisi&lt;/p&gt;&lt;p&gt;  Yazarlar:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - ve Razor ve LXQt projelerinden diğerleri&lt;/p&gt;&lt;p&gt;  Telif Hakkı: &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="111"/>
+        <source>LXQt Power Management info</source>
+        <translation>LXQT Güç Yönetimi Bilgi</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="112"/>
+        <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
+        <translation>LXQT Güç Yönetimi sistem çekmecesi simgesi &lt;i&gt;LXQT güç yönetimi ayarları&lt;/i&gt;dan etkinleştirilebilir</translation>
+    </message>
+    <message>
+        <location filename="../../../trayicon.cpp" line="114"/>
+        <source>Configure now</source>
+        <translation>Şimdi yapılandır</translation>
+    </message>
+</context>
+</TS>
