2977794f52e185bd57f3b0e183751fd6595a8226
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

---
+++ lxqt-config-powermanagement_id.ts
@@ -0,0 +1,217 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Pengaturan Pemantau Baterai</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="26"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>Aktifkan Pemantau Baterai</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="54"/>
+        <source>Warning:</source>
+        <translation>Peringatan:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="67"/>
+        <source> seconds</source>
+        <translation> detik</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="77"/>
+        <source>Level is low at:</source>
+        <translation>Level rendah di:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="90"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="111"/>
+        <source>Use icons from theme:</source>
+        <translation>Gunakan ikon dari tema:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="125"/>
+        <location filename="../../../batterywatchersettings.cpp" line="93"/>
+        <source>Preview</source>
+        <translation>Pratayang</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="162"/>
+        <source>Charging</source>
+        <translation>Mengisi daya</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="200"/>
+        <source>Discharging</source>
+        <translation>Pengosongan</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="41"/>
+        <source>When power is low then:</source>
+        <translation>Saat daya rendah maka:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.ui" line="97"/>
+        <source>Show icon:</source>
+        <translation>Tampilkan ikon:</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.cpp" line="93"/>
+        <source>built in</source>
+        <translation>bawaan</translation>
+    </message>
+    <message>
+        <location filename="../../../batterywatchersettings.cpp" line="98"/>
+        <source>Level: %1%</source>
+        <translation>Level: %1%</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulir</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="20"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>Aktifkan Pemantau Siaga</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="41"/>
+        <source>When idle then:</source>
+        <translation>Saat siaga maka:</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="60"/>
+        <source>Idle time:</source>
+        <translation>Waktu siaga:</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="73"/>
+        <source> minutes</source>
+        <translation> menit</translation>
+    </message>
+    <message>
+        <location filename="../../../idlenesswatchersettings.ui" line="86"/>
+        <source> seconds</source>
+        <translation> detik</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulir</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>Aktifkan Pemantau &amp;Lid</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="41"/>
+        <source>Action when lid is closed</source>
+        <translation>Tindakan saat lid ditutup</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="53"/>
+        <location filename="../../../lidwatchersettings.ui" line="118"/>
+        <source>On Battery</source>
+        <translation>Pada Baterai</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="61"/>
+        <source>Nothing</source>
+        <translation>Tidak ada</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="66"/>
+        <source>Suspend</source>
+        <translation>Suspensi</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="71"/>
+        <source>Hibernate</source>
+        <translation>Hibernasi</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="76"/>
+        <source>Shut down</source>
+        <translation>Matikan</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="84"/>
+        <location filename="../../../lidwatchersettings.ui" line="128"/>
+        <source>On AC</source>
+        <translation>Pada AC</translation>
+    </message>
+    <message>
+        <location filename="../../../lidwatchersettings.ui" line="97"/>
+        <source>When &amp;using external monitor</source>
+        <translation>Ketika mengunakan monitor eksternal</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="34"/>
+        <source>Power Management Settings</source>
+        <translation>Pengaturan Manajemen Daya</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="37"/>
+        <source>Battery</source>
+        <translation>Baterai</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="41"/>
+        <source>Lid</source>
+        <translation>Lid</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="45"/>
+        <source>Idle</source>
+        <translation>Siaga</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Tidak ada</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Layar kunci</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Suspensi</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Hibernasi</translation>
+    </message>
+    <message>
+        <location filename="../../../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Matikan</translation>
+    </message>
+</context>
+</TS>
