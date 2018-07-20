8bd5013b22a919deb2a43a41573f3a9c45f6234c
stefonarch <stef.binde@riseup.net>
2018-07-20 15:52:30 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (50 of 50 strings)
    
    Translation: LXQt/LXQt Powermanagement
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-powermanagement/it/

--- src/translations/lxqt-powermanagement_it.ts
+++ src/translations/lxqt-powermanagement_it.ts
@@ -200,27 +200,21 @@
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="125"/>
         <source>Suspending in %n second(s)</source>
-        <translation>
-            <numerusform>Sospensione tra %n secondi</numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Sospensione tra %n secondo</numerusform>
+        <numerusform>Sospensione tra %n secondi</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="128"/>
         <source>Hibernating in %n second(s)</source>
-        <translation>
-            <numerusform>Ibernazione tra %n secondi</numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Ibernazione tra %n secondo</numerusform>
+        <numerusform>Ibernazione tra %n secondi</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="131"/>
         <source>Shutting down in %n second(s)</source>
-        <translation>
-            <numerusform>Arresto tra %n secondi</numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Arresto tra %n secondo</numerusform>
+        <numerusform>Arresto tra %n secondi</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>PowerManagementd</name>
