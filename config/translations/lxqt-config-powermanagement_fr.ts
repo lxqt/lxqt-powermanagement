f0bb30cb21f220c3840e5c70f4316e0247ff4e66
Carl Schwan <schwancarl@protonmail.com>
2018-08-10 16:08:32 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (40 of 40 strings)
    
    Translation: LXQt/LXQt Powermanagement (Config)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-powermanagement/fr/

--- config/translations/lxqt-config-powermanagement_fr.ts
+++ config/translations/lxqt-config-powermanagement_fr.ts
@@ -216,7 +216,7 @@
     <message>
         <location filename="../helpers.cpp" line="40"/>
         <source>Turn Off monitor(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Éteindre le(s) moniteur(s)</translation>
     </message>
 </context>
 </TS>
