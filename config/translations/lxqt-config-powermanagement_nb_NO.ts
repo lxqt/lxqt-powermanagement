294b20d31f4424ea7a23d31d70696913931fdf69
Einar Mostad <einar@mostad.eu>
2018-10-24 18:54:00 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ config/translations/lxqt-config-powermanagement_nb_NO.ts
@@ -0,0 +1,222 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nb_NO">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="26"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="54"/>
+        <source>Warning:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="67"/>
+        <source> seconds</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="77"/>
+        <source>Level is low at:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="90"/>
+        <source> %</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="111"/>
+        <source>Use icons from theme:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="125"/>
+        <location filename="../batterywatchersettings.cpp" line="93"/>
+        <source>Preview</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="162"/>
+        <source>Charging</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="200"/>
+        <source>Discharging</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="41"/>
+        <source>When power is low then:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="97"/>
+        <source>Show icon:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="93"/>
+        <source>built in</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="98"/>
+        <source>Level: %1%</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="20"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="41"/>
+        <source>When idle then:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="60"/>
+        <source>Idle time:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="73"/>
+        <source> minutes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="86"/>
+        <source> seconds</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="26"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="41"/>
+        <source>Action when lid is closed</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="53"/>
+        <location filename="../lidwatchersettings.ui" line="118"/>
+        <source>On Battery</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="61"/>
+        <source>Nothing</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="66"/>
+        <source>Suspend</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="71"/>
+        <source>Hibernate</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="76"/>
+        <source>Shut down</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="84"/>
+        <location filename="../lidwatchersettings.ui" line="128"/>
+        <source>On AC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="97"/>
+        <source>When &amp;using external monitor</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="36"/>
+        <source>Power Management Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>Battery</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="43"/>
+        <source>Lid</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="47"/>
+        <source>Idle</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="40"/>
+        <source>Turn Off monitor(s)</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
