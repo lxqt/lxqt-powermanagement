1e6db7b582ba8b2b7d3cbd2c7565d62261bd2b92
Miguel A. Bouzada <mbouzada@gmail.com>
2018-12-11 09:58:23 +0000

    Translated using Weblate (Galician)
    Currently translated at 100,0% (50 of 50 strings)
    
    Translation: LXQt/LXQt Powermanagement
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-powermanagement/gl/

--- src/translations/lxqt-powermanagement_gl.ts
+++ src/translations/lxqt-powermanagement_gl.ts
@@ -200,27 +200,21 @@
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="126"/>
         <source>Suspending in %n second(s)</source>
-        <translation type="unfinished">
-            <numerusform>Suspenderase en %n segundo(s)</numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Suspenderase nun segundo</numerusform>
+        <numerusform>Suspenderase en %n segundos</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="129"/>
         <source>Hibernating in %n second(s)</source>
-        <translation type="unfinished">
-            <numerusform>Hibernará en %n segundo(s)</numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Hibernará nun segundo</numerusform>
+        <numerusform>Hibernará en %n segundos</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="132"/>
         <source>Shutting down in %n second(s)</source>
-        <translation type="unfinished">
-            <numerusform>Apagarase en %n segundo(s)</numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Apagarase nun segundo</numerusform>
+        <numerusform>Apagarase en %n segundos</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>PowerManagementd</name>
