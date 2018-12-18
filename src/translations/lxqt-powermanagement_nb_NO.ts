74798e054022f6bba38723d96b4f59f5fd3ad727
Einar Mostad <einar@mostad.eu>
2018-12-18 12:14:26 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (50 of 50 strings)
    
    Translation: LXQt/LXQt Powermanagement
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-powermanagement/nb_NO/

--- src/translations/lxqt-powermanagement_nb_NO.ts
+++ src/translations/lxqt-powermanagement_nb_NO.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../batteryhelper.cpp" line="42"/>
         <source>Empty</source>
-        <translation></translation>
+        <translation>Tom</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="44"/>
         <source>Discharging</source>
-        <translation></translation>
+        <translation>Utlader</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="46"/>
         <source>Fully charged</source>
-        <translation></translation>
+        <translation>Fult oppladet</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="49"/>
         <source>Charging</source>
-        <translation></translation>
+        <translation>Lader</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="58"/>
@@ -36,77 +36,77 @@
     <message>
         <location filename="../batteryhelper.cpp" line="62"/>
         <source>Lithium iron phosphate</source>
-        <translation></translation>
+        <translation>Lithium jernfosfat</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="64"/>
         <source>Lead acid</source>
-        <translation></translation>
+        <translation>Blyakkumulator</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="66"/>
         <source>Nickel cadmium</source>
-        <translation></translation>
+        <translation>Nikkel kadmium</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="68"/>
         <source>Nickel metal hydride</source>
-        <translation></translation>
+        <translation>Nickel metallhydrid</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="71"/>
         <source>Unknown</source>
-        <translation></translation>
+        <translation>Ukjent</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="80"/>
         <source>Personal Digital Assistant&apos;s battery</source>
-        <translation></translation>
+        <translation>PDAs batteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="82"/>
         <source>Uninterruptible Power Supply&apos;s battery</source>
-        <translation></translation>
+        <translation>Uavbrytelig strømforsynings batteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="84"/>
         <source>Primary battery</source>
-        <translation></translation>
+        <translation>Primærbatteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="86"/>
         <source>Mouse battery</source>
-        <translation></translation>
+        <translation>Musbatteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="88"/>
         <source>Keyboard battery</source>
-        <translation></translation>
+        <translation>Tastaturbatteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="90"/>
         <source>Keyboard and mouse&apos;s battery</source>
-        <translation></translation>
+        <translation>Tastaturets og musens batteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="92"/>
         <source>Camera battery</source>
-        <translation></translation>
+        <translation>Kamerabatteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="94"/>
         <source>Phone battery</source>
-        <translation></translation>
+        <translation>Telefonbatteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="96"/>
         <source>Monitor battery</source>
-        <translation></translation>
+        <translation>Skjermbatteri</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="99"/>
         <source>Unknown battery</source>
-        <translation></translation>
+        <translation>Ukjent batteri</translation>
     </message>
 </context>
 <context>
@@ -119,7 +119,7 @@
     <message>
         <location filename="../batteryinfodialog.cpp" line="24"/>
         <source>Battery Info</source>
-        <translation></translation>
+        <translation>Batteriinfo</translation>
     </message>
 </context>
 <context>
@@ -127,12 +127,12 @@
     <message>
         <location filename="../batteryinfoframe.ui" line="14"/>
         <source>Power Management</source>
-        <translation></translation>
+        <translation>Strømstyring</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="53"/>
         <source>Temperature:</source>
-        <translation></translation>
+        <translation>Temperatur:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="63"/>
@@ -142,42 +142,42 @@
     <message>
         <location filename="../batteryinfoframe.ui" line="73"/>
         <source>State:</source>
-        <translation></translation>
+        <translation>Tilstand:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="90"/>
         <source>Energy Full Design:</source>
-        <translation></translation>
+        <translation>Designet maks energi:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="107"/>
         <source>Energy Full:</source>
-        <translation></translation>
+        <translation>Maks energi:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="124"/>
         <source>Energy Now:</source>
-        <translation></translation>
+        <translation>Energi nå:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="141"/>
         <source>Energy rate:</source>
-        <translation></translation>
+        <translation>Energirate:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="165"/>
         <source>Technology:</source>
-        <translation></translation>
+        <translation>Teknologi:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="182"/>
         <source>Voltage:</source>
-        <translation></translation>
+        <translation>Spenning:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="192"/>
         <source>Vendor:</source>
-        <translation></translation>
+        <translation>Produsent:</translation>
     </message>
 </context>
 <context>
@@ -185,17 +185,17 @@
     <message>
         <location filename="../batterywatcher.cpp" line="48"/>
         <source>No battery!</source>
-        <translation></translation>
+        <translation>Ikke noe batteri!</translation>
     </message>
     <message>
         <location filename="../batterywatcher.cpp" line="49"/>
         <source>LXQt could not find data about any battery - monitoring disabled</source>
-        <translation></translation>
+        <translation>LXQt kunne ikke finne data om noe batteri - styring ble skrudd av</translation>
     </message>
     <message>
         <location filename="../batterywatcher.cpp" line="114"/>
         <source>Power low!</source>
-        <translation></translation>
+        <translation>Lite strøm!</translation>
     </message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="126"/>
@@ -221,18 +221,19 @@
     <message>
         <location filename="../powermanagementd.cpp" line="112"/>
         <source>Power Management</source>
-        <translation></translation>
+        <translation>Strømstyring</translation>
     </message>
     <message>
         <location filename="../powermanagementd.cpp" line="113"/>
         <source>You are running LXQt Power Management for the first time.
 You can configure it from settings... </source>
-        <translation></translation>
+        <translation>Du kjører LXQt strømstyring for første gang.
+Du kan konfigurere det fra innstillinger... </translation>
     </message>
     <message>
         <location filename="../powermanagementd.cpp" line="114"/>
         <source>Configure...</source>
-        <translation></translation>
+        <translation>Konfigurer...</translation>
     </message>
 </context>
 <context>
@@ -240,38 +241,38 @@ You can configure it from settings... </source>
     <message>
         <location filename="../trayicon.cpp" line="61"/>
         <source>Configure</source>
-        <translation></translation>
+        <translation>Konfigurer</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="63"/>
         <location filename="../trayicon.cpp" line="94"/>
         <source>About</source>
-        <translation></translation>
+        <translation>Om</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="65"/>
         <source>Disable icon</source>
-        <translation></translation>
+        <translation>Skru av symbol</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="95"/>
         <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
-        <translation></translation>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt strømstyring&lt;/b&gt;&lt;br/&gt;  - strømstyring for LXQt skrivebordsmiljøet&lt;/p&gt;&lt;p&gt;  Opphavsmenn:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - og andre fra Razor og LXQt prosjektene&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="113"/>
         <source>LXQt Power Management info</source>
-        <translation></translation>
+        <translation>LXQt strømstyringsinfo</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="114"/>
         <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
-        <translation></translation>
+        <translation>LXQt strømstyringssymbolet for systemtrauet kan skrus på (igjen) i &lt;i&gt;LXQt strømstyringsinnstillinger&lt;/i&gt;</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="116"/>
         <source>Configure now</source>
-        <translation></translation>
+        <translation>Konfigurer nå</translation>
     </message>
 </context>
 </TS>
