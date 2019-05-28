00e70a9338bc7fe95422117a2989205eb49c6277
Paolo Vigoroso <xpaolox@zoznam.sk>
2019-05-28 09:35:07 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (50 of 50 strings)
    
    Translation: LXQt/LXQt Powermanagement
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-powermanagement/sk_SK/

--- src/translations/lxqt-powermanagement_sk_SK.ts
+++ src/translations/lxqt-powermanagement_sk_SK.ts
@@ -6,107 +6,107 @@
     <message>
         <location filename="../batteryhelper.cpp" line="42"/>
         <source>Empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Takmer vybité</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="44"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>Vybíja sa</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="46"/>
         <source>Fully charged</source>
-        <translation type="unfinished"></translation>
+        <translation>Úplne nabité</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="49"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Nabíja sa</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="58"/>
         <source>Lithium ion</source>
-        <translation type="unfinished"></translation>
+        <translation>Lithium ion</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="60"/>
         <source>Lithium polymer</source>
-        <translation type="unfinished"></translation>
+        <translation>Lithium polymer</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="62"/>
         <source>Lithium iron phosphate</source>
-        <translation type="unfinished"></translation>
+        <translation>Lithium železo fosfát</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="64"/>
         <source>Lead acid</source>
-        <translation type="unfinished"></translation>
+        <translation>Olovená</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="66"/>
         <source>Nickel cadmium</source>
-        <translation type="unfinished"></translation>
+        <translation>Nikel kadmium</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="68"/>
         <source>Nickel metal hydride</source>
-        <translation type="unfinished"></translation>
+        <translation>Nikel metal hybrid</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="71"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Neznáma</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="80"/>
         <source>Personal Digital Assistant&apos;s battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batéria osobného digitálneho asistenta (PDA)</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="82"/>
         <source>Uninterruptible Power Supply&apos;s battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batéria zdroja neprerušovaného napájania (UPS)</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="84"/>
         <source>Primary battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Primárna batéria</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="86"/>
         <source>Mouse battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batéria myši</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="88"/>
         <source>Keyboard battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batéria klávesnice</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="90"/>
         <source>Keyboard and mouse&apos;s battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batéria klávesnice a myši</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="92"/>
         <source>Camera battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batéria fotoaparátu/kamery</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="94"/>
         <source>Phone battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Batéria telefónu</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="96"/>
         <source>Monitor battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Monitorovať batériu</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="99"/>
         <source>Unknown battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Neznáme batéria</translation>
     </message>
 </context>
 <context>
@@ -114,12 +114,12 @@
     <message>
         <location filename="../batteryinfodialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialóg</translation>
     </message>
     <message>
         <location filename="../batteryinfodialog.cpp" line="24"/>
         <source>Battery Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Informácie o batérii</translation>
     </message>
 </context>
 <context>
@@ -127,57 +127,57 @@
     <message>
         <location filename="../batteryinfoframe.ui" line="14"/>
         <source>Power Management</source>
-        <translation type="unfinished"></translation>
+        <translation>Správa napájania</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="53"/>
         <source>Temperature:</source>
-        <translation type="unfinished"></translation>
+        <translation>Teplota:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="63"/>
         <source>Type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Typ:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="73"/>
         <source>State:</source>
-        <translation type="unfinished"></translation>
+        <translation>Stav:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="90"/>
         <source>Energy Full Design:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kapacita z výroby:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="107"/>
         <source>Energy Full:</source>
-        <translation type="unfinished"></translation>
+        <translation>Zostávajúca kapacita:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="124"/>
         <source>Energy Now:</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuálne nabitie:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="141"/>
         <source>Energy rate:</source>
-        <translation type="unfinished"></translation>
+        <translation>Intenzita vybíjania:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="165"/>
         <source>Technology:</source>
-        <translation type="unfinished"></translation>
+        <translation>Technológia:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="182"/>
         <source>Voltage:</source>
-        <translation type="unfinished"></translation>
+        <translation>Napätie:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="192"/>
         <source>Vendor:</source>
-        <translation type="unfinished"></translation>
+        <translation>Výrobca:</translation>
     </message>
 </context>
 <context>
@@ -185,57 +185,58 @@
     <message>
         <location filename="../batterywatcher.cpp" line="48"/>
         <source>No battery!</source>
-        <translation type="unfinished"></translation>
+        <translation>Žiadna batéria!</translation>
     </message>
     <message>
         <location filename="../batterywatcher.cpp" line="49"/>
         <source>LXQt could not find data about any battery - monitoring disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt sa nepodarili nájsť údaje o žiadnej batérii - sledovanie je preto vypnuté</translation>
     </message>
     <message>
         <location filename="../batterywatcher.cpp" line="114"/>
         <source>Power low!</source>
-        <translation type="unfinished"></translation>
+        <translation>Takmer vybité!</translation>
     </message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="126"/>
         <source>Suspending in %n second(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Uspanie do pamäti za %n sekundu</numerusform>
+        <numerusform>Uspanie do pamäti za %n sekundy</numerusform>
+        <numerusform>Uspanie do pamäti za %n sekúnd</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="129"/>
         <source>Hibernating in %n second(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Uspanie na disk za %n sekundu</numerusform>
+        <numerusform>Uspanie na disk za %n sekundy</numerusform>
+        <numerusform>Uspanie na disk za %n sekúnd</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="132"/>
         <source>Shutting down in %n second(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Vypnutie za %n sekundu</numerusform>
+        <numerusform>Vypnutie za %n sekundy</numerusform>
+        <numerusform>Vypnutie za %n sekúnd</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>PowerManagementd</name>
     <message>
         <location filename="../powermanagementd.cpp" line="112"/>
         <source>Power Management</source>
-        <translation type="unfinished"></translation>
+        <translation>Správca napájania</translation>
     </message>
     <message>
         <location filename="../powermanagementd.cpp" line="113"/>
         <source>You are running LXQt Power Management for the first time.
 You can configure it from settings... </source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt správa napájania je spustená prvý krát.
+Parametre je možné upraviť v Nastaveniach… </translation>
     </message>
     <message>
         <location filename="../powermanagementd.cpp" line="114"/>
         <source>Configure...</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastaviť…</translation>
     </message>
 </context>
 <context>
@@ -243,38 +244,38 @@ You can configure it from settings... </source>
     <message>
         <location filename="../trayicon.cpp" line="61"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastaviť</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="63"/>
         <location filename="../trayicon.cpp" line="94"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O aplikácii</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="65"/>
         <source>Disable icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Nezobrazovať ikonu</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="95"/>
         <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;  &lt;b&gt;Správa napájania&lt;/b&gt;&lt;br/&gt;  – správa napájania pre pracovné prostredie LXQt&lt;/p&gt;&lt;p&gt;  Autori:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; – a ďalší z projektov Razor a LXQt&lt;/p&gt;&lt;p&gt;  Autorské práva &amp;copy; 2012-2014&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="113"/>
         <source>LXQt Power Management info</source>
-        <translation type="unfinished"></translation>
+        <translation>Informácie zo spravy napájania LXQt</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="114"/>
         <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobrazovanie ikony správy napájania v systémovom panely je možné (znovu) zapnúť v &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="116"/>
         <source>Configure now</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastaviť teraz</translation>
     </message>
 </context>
 </TS>
