73cbce2353d5083cb21d98d31220c3458c82dd8a
milotype <43657314+milotype@users.noreply.github.com>
2020-05-30 22:17:32 +0200

    Croatian translation

---
+++ config/translations/lxqt-config-powermanagement_hr.ts
@@ -0,0 +1,212 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>Praćenje stanja baterije – Postavke</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="26"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>&amp;Aktiviraj praćenje stanja baterije</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="54"/>
+        <source>Warning:</source>
+        <translation>Upozorenje:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="67"/>
+        <source> seconds</source>
+        <translation> s</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="77"/>
+        <source>Level is low at:</source>
+        <translation>Razina je niska pri:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="90"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="111"/>
+        <source>Use icons from theme:</source>
+        <translation>Koristi ikone teme:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="125"/>
+        <location filename="../batterywatchersettings.cpp" line="93"/>
+        <source>Preview</source>
+        <translation>Pregled</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="162"/>
+        <source>Charging</source>
+        <translation>Punjenje</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="200"/>
+        <source>Discharging</source>
+        <translation>Pražnjenje</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="41"/>
+        <source>When power is low then:</source>
+        <translation>Kad je razina niska, onda:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="97"/>
+        <source>Show icon:</source>
+        <translation>Prikaži ikonu:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="93"/>
+        <source>built in</source>
+        <translation>ugrađeno</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="98"/>
+        <source>Level: %1%</source>
+        <translation>Razina: %1 %</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="17"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>&amp;Aktiviraj praćenje stanja mirovanja</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="38"/>
+        <source>When idle then:</source>
+        <translation>Kad miruje, onda:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="57"/>
+        <source>Idle time:</source>
+        <translation>Vrijeme mirovanja:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="70"/>
+        <source> minutes</source>
+        <translation> min</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="83"/>
+        <source> seconds</source>
+        <translation> s</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="23"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>Aktiviraj &amp;praćenje stanja poklopca</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="38"/>
+        <source>Action when lid is closed</source>
+        <translation>Radnja kad je poklopac zatvoren</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="50"/>
+        <location filename="../lidwatchersettings.ui" line="115"/>
+        <source>On Battery</source>
+        <translation>Napajanje baterijom</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="58"/>
+        <source>Nothing</source>
+        <translation>Ništa</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="63"/>
+        <source>Suspend</source>
+        <translation>Pripravno stanje</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="68"/>
+        <source>Hibernate</source>
+        <translation>Stanje mirovanja</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="73"/>
+        <source>Shut down</source>
+        <translation>Isključi računalo</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="81"/>
+        <location filename="../lidwatchersettings.ui" line="125"/>
+        <source>On AC</source>
+        <translation>Napajanje strujom</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="94"/>
+        <source>When &amp;using external monitor</source>
+        <translation>Kad se &amp;koristi vanjski monitor</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="36"/>
+        <source>Power Management Settings</source>
+        <translation>Upravljanje energijom – Postavke</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>Battery</source>
+        <translation>Baterija</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="43"/>
+        <source>Lid</source>
+        <translation>Poklopac</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="47"/>
+        <source>Idle</source>
+        <translation>Mirovanje</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>Ništa</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="36"/>
+        <source>Lock screen</source>
+        <translation>Zaključaj ekran</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="37"/>
+        <source>Suspend</source>
+        <translation>Pripravno stanje</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Hibernate</source>
+        <translation>Stanje mirovanja</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="39"/>
+        <source>Shutdown</source>
+        <translation>Isključi računalo</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="40"/>
+        <source>Turn Off monitor(s)</source>
+        <translation>Ugasi monitore</translation>
+    </message>
+</context>
+</TS>
