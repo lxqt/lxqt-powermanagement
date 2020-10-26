3960b905cdfbb3b760200df5ee2403180e825877
Ettore Atalan <atalanttore@googlemail.com>
2020-10-26 22:49:55 +0100

    Weblate commits
    Co-authored-by: Ettore Atalan <atalanttore@googlemail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/de/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement

--- src/translations/lxqt-powermanagement_de.ts
+++ src/translations/lxqt-powermanagement_de.ts
@@ -227,17 +227,17 @@
     <message>
         <location filename="../powerbutton.cpp" line="72"/>
         <source>Power off key action</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausschalt-Tastenaktion</translation>
     </message>
     <message>
         <location filename="../powerbutton.cpp" line="79"/>
         <source>Suspend key action</source>
-        <translation type="unfinished"></translation>
+        <translation>Bereitschaftszustand-Tastenaktion</translation>
     </message>
     <message>
         <location filename="../powerbutton.cpp" line="86"/>
         <source>Hibernate key action</source>
-        <translation type="unfinished"></translation>
+        <translation>Ruhezustand-Tastenaktion</translation>
     </message>
 </context>
 <context>
