1a230317dd7c98ef824459e0a3ecf3f490c8b826
João Paulo Pulga <pulgovisk@protonmail.com>
2020-10-28 13:06:12 +0100

    Weblate commits
    Co-authored-by: João Paulo Pulga <pulgovisk@protonmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/pt_BR/
    Translation: LXQt Configuration/lxqt-powermanagement

--- src/translations/lxqt-powermanagement_pt_BR.ts
+++ src/translations/lxqt-powermanagement_pt_BR.ts
@@ -227,17 +227,17 @@
     <message>
         <location filename="../powerbutton.cpp" line="72"/>
         <source>Power off key action</source>
-        <translation type="unfinished"></translation>
+        <translation>Ação do botão desligar</translation>
     </message>
     <message>
         <location filename="../powerbutton.cpp" line="79"/>
         <source>Suspend key action</source>
-        <translation type="unfinished"></translation>
+        <translation>Ação do botão suspender</translation>
     </message>
     <message>
         <location filename="../powerbutton.cpp" line="86"/>
         <source>Hibernate key action</source>
-        <translation type="unfinished"></translation>
+        <translation>Ação do botão hibernar</translation>
     </message>
 </context>
 <context>
