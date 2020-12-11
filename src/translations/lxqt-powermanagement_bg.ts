81ff2dd316f3c1614eadd7d2c21fb42eb777dcc2
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-12-11 18:08:57 +0100

    Translations update from Weblate (#220)
    * Weblate commits
    
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagement-module-name/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/bg/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry
    Translation: LXQt Configuration/lxqt-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement Module Name
    
    * Weblate commits
    
    Co-authored-by: Teemu Sirviö <teemu.sirvio@fmi.fi>
    
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Co-authored-by: Teemu Sirviö <teemu.sirvio@fmi.fi>

---
+++ src/translations/lxqt-powermanagement_bg.ts
@@ -0,0 +1,302 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>BatteryHelper</name>
+    <message>
+        <location filename="../batteryhelper.cpp" line="42"/>
+        <source>Empty</source>
+        <translation>Празна</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="44"/>
+        <source>Discharging</source>
+        <translation>Разреждане</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="46"/>
+        <source>Fully charged</source>
+        <translation>Напълно заредена</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="49"/>
+        <source>Charging</source>
+        <translation>Зареждане</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="58"/>
+        <source>Lithium ion</source>
+        <translation>Литиевойонна</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="60"/>
+        <source>Lithium polymer</source>
+        <translation>Литиевополимерна</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="62"/>
+        <source>Lithium iron phosphate</source>
+        <translation>Литиевойонна фосфатна</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="64"/>
+        <source>Lead acid</source>
+        <translation>Оловнокисела</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="66"/>
+        <source>Nickel cadmium</source>
+        <translation>Никел-кадмиева</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="68"/>
+        <source>Nickel metal hydride</source>
+        <translation>Никел-металхидридна</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="71"/>
+        <source>Unknown</source>
+        <translation>Неизвестна</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="80"/>
+        <source>Personal Digital Assistant&apos;s battery</source>
+        <translation>Батерия на PDA</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="82"/>
+        <source>Uninterruptible Power Supply&apos;s battery</source>
+        <translation>Батерия на UPS</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="84"/>
+        <source>Primary battery</source>
+        <translation>Основна батерия</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="86"/>
+        <source>Mouse battery</source>
+        <translation>Батерия на мишка</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="88"/>
+        <source>Keyboard battery</source>
+        <translation>Батерия на клавиатура</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="90"/>
+        <source>Keyboard and mouse&apos;s battery</source>
+        <translation>Батерия на клавиатура и мишка</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="92"/>
+        <source>Camera battery</source>
+        <translation>Батерия на камера</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="94"/>
+        <source>Phone battery</source>
+        <translation>Батерия на телефон</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="96"/>
+        <source>Monitor battery</source>
+        <translation>Батерия на монитора</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="99"/>
+        <source>Unknown battery</source>
+        <translation>Батерия неизвестна</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoDialog</name>
+    <message>
+        <location filename="../batteryinfodialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfodialog.cpp" line="24"/>
+        <source>Battery Info</source>
+        <translation>Информация за батерия</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoFrame</name>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>Управление на захранването</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="53"/>
+        <source>Temperature:</source>
+        <translation>Температура:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="63"/>
+        <source>Type:</source>
+        <translation>Тип:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="73"/>
+        <source>State:</source>
+        <translation>Състояние:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="90"/>
+        <source>Energy Full Design:</source>
+        <translation>Енергиен дизайн:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="107"/>
+        <source>Energy Full:</source>
+        <translation>Енергия при пълно зареждане:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="124"/>
+        <source>Energy Now:</source>
+        <translation>Енергия в момента:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="141"/>
+        <source>Energy rate:</source>
+        <translation>Енергийна норма:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="165"/>
+        <source>Technology:</source>
+        <translation>Технология:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="182"/>
+        <source>Voltage:</source>
+        <translation>Волтаж:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="192"/>
+        <source>Vendor:</source>
+        <translation>Vendor:</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="48"/>
+        <source>No battery!</source>
+        <translation>Няма батерия!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQt не може да намери данни за батерия - мониторирането е деактивирано</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="114"/>
+        <source>Power low!</source>
+        <translation>Ниско ниво на батерията!</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="126"/>
+        <source>Suspending in %n second(s)</source>
+        <translation>
+            <numerusform>Приспиване след %n s</numerusform>
+            <numerusform>Приспиване след %n s</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="129"/>
+        <source>Hibernating in %n second(s)</source>
+        <translation>
+            <numerusform>Хибернация след %n s</numerusform>
+            <numerusform>Хибернация след %n s</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="132"/>
+        <source>Shutting down in %n second(s)</source>
+        <translation>
+            <numerusform>Изключване след %n s</numerusform>
+            <numerusform>Изключване след %n s</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PowerButton</name>
+    <message>
+        <location filename="../powerbutton.cpp" line="72"/>
+        <source>Power off key action</source>
+        <translation>Клавишна комбинация за изключване</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="79"/>
+        <source>Suspend key action</source>
+        <translation>Клавишна комбинация за приспиване</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="86"/>
+        <source>Hibernate key action</source>
+        <translation>Клавишна комбинация за хибернация</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="115"/>
+        <source>Power Management</source>
+        <translation>Управление на захранването</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="116"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>Изпълнявате Управление на захранването за първи път.
+Можете да го конфигурирате от настройките ... </translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="117"/>
+        <source>Configure...</source>
+        <translation>Конфигуриране...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="61"/>
+        <source>Configure</source>
+        <translation>Конфигуриране</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="63"/>
+        <location filename="../trayicon.cpp" line="94"/>
+        <source>About</source>
+        <translation>Относно</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="65"/>
+        <source>Disable icon</source>
+        <translation>Деактивиране на икона</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="95"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt Управление на захранването&lt;/b&gt;&lt;br/&gt;  - Управление на захранването на LXQt работна среда&lt;/p&gt;&lt;p&gt;  Автори:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - и други от Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="113"/>
+        <source>LXQt Power Management info</source>
+        <translation>Управление на захранването Инфо</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="114"/>
+        <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
+        <translation>Иконата за зоната на системния обмен може да бъде активирана в&lt;i&gt;Настройки-&gt;LXQt-&gt;Управление на захранването&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="116"/>
+        <source>Configure now</source>
+        <translation>Конфигурирай сега</translation>
+    </message>
+</context>
+</TS>
