43da6b5788067e26acc310cadd1820a0aa7f8602
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-12-20 17:59:39 +0100

    Translations update from Weblate (#221)
    * Weblate commits
    
    Co-authored-by: Teemu Sirviö <teemu.sirvio@fmi.fi>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/fi/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement
    
    * Weblate commits
    
    Co-authored-by: Standreas <standreas@riseup.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/bg/
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry
    
    Co-authored-by: Teemu Sirviö <teemu.sirvio@fmi.fi>
    Co-authored-by: Standreas <standreas@riseup.net>

--- config/translations/lxqt-config-powermanagement_fi.ts
+++ config/translations/lxqt-config-powermanagement_fi.ts
@@ -6,73 +6,73 @@
     <message>
         <location filename="../batterywatchersettings.ui" line="14"/>
         <source>Battery Watcher Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Akunvalvonnan asetukset</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="26"/>
         <source>E&amp;nable Battery Watcher</source>
-        <translation type="unfinished"></translation>
+        <translation>Ota käyttöön akun valvonta</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="54"/>
         <source>Warning:</source>
-        <translation type="unfinished"></translation>
+        <translation>Varoitus:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="67"/>
         <source> seconds</source>
-        <translation type="unfinished"></translation>
+        <translation> sekuntia</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="77"/>
         <source>Level is low at:</source>
-        <translation type="unfinished"></translation>
+        <translation>Latauksen alhainen taso:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="90"/>
         <source> %</source>
-        <translation type="unfinished"></translation>
+        <translation> %</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="111"/>
         <source>Use icons from theme:</source>
-        <translation type="unfinished"></translation>
+        <translation>Käytä teeman kuvakkeita:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="125"/>
         <location filename="../batterywatchersettings.cpp" line="93"/>
         <source>Preview</source>
-        <translation type="unfinished"></translation>
+        <translation>Esikatselu</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="162"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Lataa</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="200"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>Purkaantuu</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="41"/>
         <source>When power is low then:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kun virta on alhainen:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="97"/>
         <source>Show icon:</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytä kuvake:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="93"/>
         <source>built in</source>
-        <translation type="unfinished"></translation>
+        <translation>sisäänrakennettu</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="98"/>
         <source>Level: %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>Taso: %1%</translation>
     </message>
 </context>
 <context>
@@ -80,58 +80,58 @@
     <message>
         <location filename="../idlenesswatchersettings.ui" line="17"/>
         <source>Enab&amp;le Idleness Watcher</source>
-        <translation type="unfinished"></translation>
+        <translation>Ota käyttöön joutilaan ajan valvonta</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="34"/>
         <source>When idle then:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kun käyttämättömänä:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="53"/>
         <location filename="../idlenesswatchersettings.ui" line="123"/>
         <source>Idle time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Joutilaana olo aika:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="66"/>
         <source> minutes</source>
-        <translation type="unfinished"></translation>
+        <translation> minuuttia</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="79"/>
         <source> seconds</source>
-        <translation type="unfinished"></translation>
+        <translation> sekuntia</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="94"/>
         <source>Enable Backlight Change</source>
-        <translation type="unfinished"></translation>
+        <translation>Ota käyttöön taustavalon muutos</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="103"/>
         <source>Backlight (%):</source>
-        <translation type="unfinished"></translation>
+        <translation>Taustavalo (%):</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="130"/>
         <source>Minutes:Seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>Minuuttia:sekuntia</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="150"/>
         <source>mm:ss</source>
-        <translation type="unfinished"></translation>
+        <translation>mm:ss</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="157"/>
         <source>Check backlight</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarkista taustavalo</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="164"/>
         <source>On battery discharging </source>
-        <translation type="unfinished"></translation>
+        <translation>Kun akku purkaantuu </translation>
     </message>
 </context>
 <context>
@@ -139,49 +139,49 @@
     <message>
         <location filename="../lidwatchersettings.ui" line="23"/>
         <source>Enable &amp;Lid Watcher</source>
-        <translation type="unfinished"></translation>
+        <translation>Ota käyttöön kannen valvonta</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="38"/>
         <source>Action when lid is closed</source>
-        <translation type="unfinished"></translation>
+        <translation>Toiminto kun kansi suljetaan</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="50"/>
         <location filename="../lidwatchersettings.ui" line="115"/>
         <source>On Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Akkuvirralla</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="58"/>
         <source>Nothing</source>
-        <translation type="unfinished"></translation>
+        <translation>Ei mitään</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="63"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Lepotila</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="68"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Horrostila</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="73"/>
         <source>Shut down</source>
-        <translation type="unfinished"></translation>
+        <translation>Sammuta</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="81"/>
         <location filename="../lidwatchersettings.ui" line="125"/>
         <source>On AC</source>
-        <translation type="unfinished"></translation>
+        <translation>Verkkovirralla</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="94"/>
         <source>When &amp;using external monitor</source>
-        <translation type="unfinished"></translation>
+        <translation>Kun ulkoisia näyttöjä on käytössä</translation>
     </message>
 </context>
 <context>
@@ -189,27 +189,27 @@
     <message>
         <location filename="../mainwindow.cpp" line="37"/>
         <source>Power Management Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Virranhallinnan asetukset</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="40"/>
         <source>Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Akku</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Lid</source>
-        <translation type="unfinished"></translation>
+        <translation>Kansi</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Idle</source>
-        <translation type="unfinished"></translation>
+        <translation>Joutilas</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="52"/>
         <source>Power keys</source>
-        <translation type="unfinished"></translation>
+        <translation>Virtanäppäimet</translation>
     </message>
 </context>
 <context>
@@ -222,22 +222,22 @@
     <message>
         <location filename="../powerkeyssettings.ui" line="20"/>
         <source>Handle Power Keys</source>
-        <translation type="unfinished"></translation>
+        <translation>Käsittele virtanäppäimet</translation>
     </message>
     <message>
         <location filename="../powerkeyssettings.ui" line="26"/>
         <source>Power Key Action</source>
-        <translation type="unfinished"></translation>
+        <translation>VIrtanäppäimen toiminto</translation>
     </message>
     <message>
         <location filename="../powerkeyssettings.ui" line="42"/>
         <source>Suspend Key Action</source>
-        <translation type="unfinished"></translation>
+        <translation>Lepotila-näppäimen toiminto</translation>
     </message>
     <message>
         <location filename="../powerkeyssettings.ui" line="49"/>
         <source>Hibernate Key Action</source>
-        <translation type="unfinished"></translation>
+        <translation>Horrostilanäppäimen toiminto</translation>
     </message>
 </context>
 <context>
@@ -245,37 +245,37 @@
     <message>
         <location filename="../helpers.cpp" line="35"/>
         <source>Nothing</source>
-        <translation type="unfinished"></translation>
+        <translation>Ei mitään</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="36"/>
         <source>Ask</source>
-        <translation type="unfinished"></translation>
+        <translation>Kysy</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="37"/>
         <source>Lock screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Lukitse näyttö</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="38"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Siirry lepotilaan</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="39"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Siirry horrostilaan</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="40"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>Sammuta</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="41"/>
         <source>Turn Off monitor(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Sammuta näyttö/näytöt</translation>
     </message>
 </context>
 </TS>
