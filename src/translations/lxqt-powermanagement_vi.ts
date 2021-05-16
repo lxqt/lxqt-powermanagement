1e0f24eb61e67b05c5156dd29d8622917f94132e
dimuaconheo <dimuaconheo@gmail.com>
2021-05-16 17:07:27 +0200

    Weblate commits
    Co-authored-by: dimuaconheo <dimuaconheo@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagement-module-name/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/vi/
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry
    Translation: LXQt Configuration/lxqt-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement Module Name

---
+++ src/translations/lxqt-powermanagement_vi.ts
@@ -0,0 +1,299 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi">
+<context>
+    <name>BatteryHelper</name>
+    <message>
+        <location filename="../batteryhelper.cpp" line="42"/>
+        <source>Empty</source>
+        <translation>Trống</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="44"/>
+        <source>Discharging</source>
+        <translation>Đang xả pin</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="46"/>
+        <source>Fully charged</source>
+        <translation>Sạc đầy</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="49"/>
+        <source>Charging</source>
+        <translation>Đang sạc</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="58"/>
+        <source>Lithium ion</source>
+        <translation>Lithium ion</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="60"/>
+        <source>Lithium polymer</source>
+        <translation>Lithium polymer</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="62"/>
+        <source>Lithium iron phosphate</source>
+        <translation>Lithium sắt phosphate</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="64"/>
+        <source>Lead acid</source>
+        <translation>Axít chì</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="66"/>
+        <source>Nickel cadmium</source>
+        <translation>Nickel cadmium</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="68"/>
+        <source>Nickel metal hydride</source>
+        <translation>Hydride kim loại nickel</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="71"/>
+        <source>Unknown</source>
+        <translation>Không rõ</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="80"/>
+        <source>Personal Digital Assistant&apos;s battery</source>
+        <translation>Pin Trợ lí Kĩ thuật số Cá nhân</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="82"/>
+        <source>Uninterruptible Power Supply&apos;s battery</source>
+        <translation>Pin Nguồn cung Năng lượng Không ngắt quãng</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="84"/>
+        <source>Primary battery</source>
+        <translation>Pin chính</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="86"/>
+        <source>Mouse battery</source>
+        <translation>Pin chuột</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="88"/>
+        <source>Keyboard battery</source>
+        <translation>Pin bàn phím</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="90"/>
+        <source>Keyboard and mouse&apos;s battery</source>
+        <translation>Pin bàn phím và chuột</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="92"/>
+        <source>Camera battery</source>
+        <translation>Pin camera</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="94"/>
+        <source>Phone battery</source>
+        <translation>Pin điện thoại</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="96"/>
+        <source>Monitor battery</source>
+        <translation>Pin màn hình</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="99"/>
+        <source>Unknown battery</source>
+        <translation>Pin không rõ</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoDialog</name>
+    <message>
+        <location filename="../batteryinfodialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Hộp thoại</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfodialog.cpp" line="24"/>
+        <source>Battery Info</source>
+        <translation>Thông tin Pin</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoFrame</name>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>Quản lí Năng lượng</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="53"/>
+        <source>Temperature:</source>
+        <translation>Nhiệt độ:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="63"/>
+        <source>Type:</source>
+        <translation>Loại:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="73"/>
+        <source>State:</source>
+        <translation>Tình trạng:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="90"/>
+        <source>Energy Full Design:</source>
+        <translation>Thiết kế Năng lượng Đầy đủ:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="107"/>
+        <source>Energy Full:</source>
+        <translation>Năng lượng Đầy:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="124"/>
+        <source>Energy Now:</source>
+        <translation>Năng lượng Hiện tại:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="141"/>
+        <source>Energy rate:</source>
+        <translation>Tỉ lệ năng lượng:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="165"/>
+        <source>Technology:</source>
+        <translation>Công nghệ:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="182"/>
+        <source>Voltage:</source>
+        <translation>Mức vôn:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="192"/>
+        <source>Vendor:</source>
+        <translation>Hãng:</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="48"/>
+        <source>No battery!</source>
+        <translation>Không có pin!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQt không thể tìm thấy được dữ liệu về bất kì pin nào - theo dõi đã bị tắt</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="114"/>
+        <source>Power low!</source>
+        <translation>Pin yếu!</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="126"/>
+        <source>Suspending in %n second(s)</source>
+        <translation>
+            <numerusform>Tạm ngưng trong %n giây</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="129"/>
+        <source>Hibernating in %n second(s)</source>
+        <translation>
+            <numerusform>Ngủ đông trong %n giây</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="132"/>
+        <source>Shutting down in %n second(s)</source>
+        <translation>
+            <numerusform>Tắt máy trong %n giây</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PowerButton</name>
+    <message>
+        <location filename="../powerbutton.cpp" line="72"/>
+        <source>Power off key action</source>
+        <translation>Hành động phím tắt nguồn</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="79"/>
+        <source>Suspend key action</source>
+        <translation>Hành động phím tạm ngưng</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="86"/>
+        <source>Hibernate key action</source>
+        <translation>Hành động phím ngủ đông</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="115"/>
+        <source>Power Management</source>
+        <translation>Quản lí Năng lượng</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="116"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>Bạn đang chạy Quản lí Năng lượng LXQt lần đầu tiên.
+Bạn có thể tùy chỉnh nó trong cài đặt... </translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="117"/>
+        <source>Configure...</source>
+        <translation>Tùy chỉnh...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="60"/>
+        <source>Configure</source>
+        <translation>Tùy chỉnh</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="62"/>
+        <location filename="../trayicon.cpp" line="93"/>
+        <source>About</source>
+        <translation>Về</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="64"/>
+        <source>Disable icon</source>
+        <translation>Tắt biểu tượng</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="94"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;Quản lí Năng lượng LXQt &lt;/b&gt;&lt;br/&gt;  - Quản lí Năng lượng cho Mội trường Desktop LXQt&lt;/p&gt;&lt;p&gt;  Tác giả:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - và số khác từ dự án Razor và LXQt&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="112"/>
+        <source>LXQt Power Management info</source>
+        <translation>Thông tin Quản lí Năng lượng LXQt</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="113"/>
+        <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
+        <translation>Biểu tượng khay hệ thống Quản lí Năng lượng LXQt có thể được bật (lại) trong &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="115"/>
+        <source>Configure now</source>
+        <translation>Tùy chỉnh ngay bây giờ</translation>
+    </message>
+</context>
+</TS>
