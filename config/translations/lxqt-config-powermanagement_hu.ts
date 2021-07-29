bcf0435dd6fe3d5b0c09f287e53268b708b64204
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-07-29 07:23:05 +0200

    Translations update from Weblate (#273)
    * Weblate commits
    
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/nb_NO/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    
    * Weblate commits
    
    Co-authored-by: Ács Zoltán <acszoltan111@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/hu/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Co-authored-by: Ács Zoltán <acszoltan111@gmail.com>

--- config/translations/lxqt-config-powermanagement_hu.ts
+++ config/translations/lxqt-config-powermanagement_hu.ts
@@ -100,7 +100,7 @@
     <message>
         <location filename="../idlenesswatchersettings.ui" line="90"/>
         <source>Battery Idle time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Akkumulátor tétlenségi idő:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="131"/>
@@ -122,18 +122,18 @@
     <message>
         <location filename="../idlenesswatchersettings.ui" line="34"/>
         <source>When idle on AC:</source>
-        <translation type="unfinished"></translation>
+        <translation>Amikor külső tápon tétlen:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="53"/>
         <source>AC Idle time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Külső táp tétlenségi idő:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="63"/>
         <location filename="../idlenesswatchersettings.ui" line="97"/>
         <source>Minutes:Seconds (min: 01:30)</source>
-        <translation type="unfinished"></translation>
+        <translation>Perc:másodperc (minimum: 01:30)</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="185"/>
@@ -148,7 +148,7 @@
     <message>
         <location filename="../idlenesswatchersettings.ui" line="208"/>
         <source>Disable idleness checks when fullscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>Tétlenségi ellenőrzés kikapcsolás teljes képernyős alkalmazás esetén</translation>
     </message>
 </context>
 <context>
