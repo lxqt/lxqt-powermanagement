0b121cb242002143cfda227314c985826f437606
Weblate <noreply@weblate.org>
2022-05-13 15:09:32 +0200

    Weblate commits
    Co-authored-by: runtav-gyz <runtav.gyz@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/bg/
    Translation: LXQt Configuration/lxqt-config-powermanagement

--- config/translations/lxqt-config-powermanagement_bg.ts
+++ config/translations/lxqt-config-powermanagement_bg.ts
@@ -86,7 +86,7 @@
         <location filename="../batterywatchersettings.cpp" line="44"/>
         <source>Battery with percentage and background</source>
         <comment>icons</comment>
-        <translation type="unfinished"></translation>
+        <translation>Батерия с проценти и фон</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="45"/>
