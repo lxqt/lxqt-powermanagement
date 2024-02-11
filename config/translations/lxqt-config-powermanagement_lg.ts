1f99056c19f57424cb4cf4e8a49de82f31c67a69
Weblate <noreply@weblate.org>
2024-02-11 03:23:17 +0100

    Weblate commits
    Co-authored-by: Anggiramadyansyah <anggirrr31@gmail.com>
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/id/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/lg/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry

--- config/translations/lxqt-config-powermanagement_lg.ts
+++ config/translations/lxqt-config-powermanagement_lg.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="id">
+<TS version="2.1" language="lg">
 <context>
     <name>BatteryWatcherSettings</name>
     <message>
         <location filename="../batterywatchersettings.ui" line="14"/>
         <source>Battery Watcher Settings</source>
-        <translation>Pengaturan Pemantau Baterai</translation>
+        <translation>Enteekateeka z&apos;ekirabiriza lyanda</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="26"/>
         <source>E&amp;nable Battery Watcher</source>
-        <translation>Aktifkan Pemantau Baterai</translation>
+        <translation>Ta&amp;ndika ekirabiriza lyanda</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="54"/>
         <source>Warning:</source>
-        <translation>Peringatan:</translation>
+        <translation>Labulira ku:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="67"/>
         <source> seconds</source>
-        <translation> detik</translation>
+        <translation> sikonda</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="77"/>
         <source>Level is low at:</source>
-        <translation>Level rendah di:</translation>
+        <translation>Omuliro gubalibwe ng&apos;omutono bwe gutuuka ku:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="90"/>
@@ -36,73 +36,73 @@
     <message>
         <location filename="../batterywatchersettings.ui" line="111"/>
         <source>Icon:</source>
-        <translation>Ikon:</translation>
+        <translation>Kafaananyi:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="121"/>
         <location filename="../batterywatchersettings.cpp" line="104"/>
         <source>Preview</source>
-        <translation>Pratayang</translation>
+        <translation>Kyegezemu</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="158"/>
         <source>Charging</source>
-        <translation>Mengisi daya</translation>
+        <translation>Lijjuzibwa</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="196"/>
         <source>Discharging</source>
-        <translation>Pengosongan</translation>
+        <translation>Linywebwa</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="41"/>
         <source>When power is low then:</source>
-        <translation>Saat daya rendah maka:</translation>
+        <translation>Omuliro bwe gubeera mutono:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.ui" line="97"/>
         <source>Show icon:</source>
-        <translation>Tampilkan ikon:</translation>
+        <translation>Wabeewo akafaananyi:</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="41"/>
         <source>from theme</source>
         <comment>icons</comment>
-        <translation>Dari tema</translation>
+        <translation>ka mu lulyo lumu n&apos;obuli awakolerwa</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="42"/>
         <source>Circle</source>
         <comment>icons</comment>
-        <translation>Lingkaran</translation>
+        <translation>Namuziga</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="43"/>
         <source>Circle with percentage</source>
         <comment>icons</comment>
-        <translation>Lingkaran dengan persentase</translation>
+        <translation>Namuziga n&apos;ekigero kya kikumi</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="44"/>
         <source>Battery with percentage and background</source>
         <comment>icons</comment>
-        <translation>Batre dengan persentase dengan latar belakang</translation>
+        <translation>Lyanda bwaliriro n&apos;ekigero kya kikumi</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="45"/>
         <source>Battery with percentage</source>
         <comment>icons</comment>
-        <translation>Batre dengan persentase</translation>
+        <translation>Lyanda n&apos;ekigero kya kikumi</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="104"/>
         <source>built in</source>
-        <translation>bawaan</translation>
+        <translation>kijja kyokka</translation>
     </message>
     <message>
         <location filename="../batterywatchersettings.cpp" line="109"/>
         <source>Level: %1%</source>
-        <translation>Level: %1%</translation>
+        <translation>Muliro: %1%</translation>
     </message>
 </context>
 <context>
@@ -110,75 +110,75 @@
     <message>
         <location filename="../idlenesswatchersettings.ui" line="17"/>
         <source>Enab&amp;le Idleness Watcher</source>
-        <translation>Aktifkan Pemantau Siaga</translation>
+        <translation>Tandika eki&amp;rabiriza kusiriikirira</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="151"/>
         <source>Idle time:</source>
-        <translation>Waktu siaga:</translation>
+        <translation>Akasiriikiriro:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="122"/>
         <source>Enable Backlight Change</source>
-        <translation>Aktifkan Perubahan Lampu Latar</translation>
+        <translation>Kkiriza okumulisa kw&apos;olutimbe okukyuka</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="83"/>
         <source>When idle on Battery:</source>
-        <translation type="unfinished">Saat baterai menganggur:</translation>
+        <translation>Mu kasiriikiriro nga eri ku lyanda:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="90"/>
         <source>Battery Idle time:</source>
-        <translation type="unfinished">Waktu menganggur baterai:</translation>
+        <translation>Akasiriikiriro nga eri ku lyanda:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="131"/>
         <source>Backlight (%):</source>
-        <translation>Lampur latar (%):</translation>
+        <translation>Okumulisa (%):</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="158"/>
         <source>Minutes:Seconds</source>
-        <translation>Menit:Detik</translation>
+        <translation>Ddakiika:Sikonda</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="76"/>
         <location filename="../idlenesswatchersettings.ui" line="110"/>
         <location filename="../idlenesswatchersettings.ui" line="178"/>
         <source>mm:ss</source>
-        <translation type="unfinished">Jam:detik</translation>
+        <translation>mm:ss</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="34"/>
         <source>When idle on AC:</source>
-        <translation type="unfinished"></translation>
+        <translation>Mu kasiriikiriro nga eri ku masanyalaze:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="53"/>
         <source>AC Idle time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Akasiriikiriro nga eri ku masanyalaze:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="63"/>
         <location filename="../idlenesswatchersettings.ui" line="97"/>
         <source>Minutes:Seconds (min: 01:30)</source>
-        <translation type="unfinished">Menit:Detik (min: 01:30)</translation>
+        <translation>Ddakiika:Sikonda (ekitono: 01:30)</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="185"/>
         <source>Check backlight</source>
-        <translation>Memerika Lampu latar</translation>
+        <translation>Gezesa okumulisa olutimbe</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="192"/>
         <source>On battery discharging </source>
-        <translation>Pada pengurasan batery </translation>
+        <translation>Okumulisa kukendeere nga kompyuta enywa lyanda </translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="208"/>
         <source>Disable idleness checks when fullscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>Yimiriza okulabiriza okusiriikirira nga waliwo puloguramu ebuna olutimba</translation>
     </message>
 </context>
 <context>
@@ -186,49 +186,49 @@
     <message>
         <location filename="../lidwatchersettings.ui" line="23"/>
         <source>Enable &amp;Lid Watcher</source>
-        <translation>Aktifkan Pemantau &amp;Lid</translation>
+        <translation>Tandika &amp;ekirabiriza ekisaanikira</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="38"/>
         <source>Action when lid is closed:</source>
-        <translation>Tindakan saat lid ditutup:</translation>
+        <translation>Ekikolebwa nga ekisaanikira kiggadwa:</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="50"/>
         <location filename="../lidwatchersettings.ui" line="115"/>
         <source>On Battery:</source>
-        <translation>Pada Baterai:</translation>
+        <translation>Nga eri ku lyanda:</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="58"/>
         <source>Nothing</source>
-        <translation>Tidak ada</translation>
+        <translation>Tewali</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="63"/>
         <source>Suspend</source>
-        <translation>Suspensi</translation>
+        <translation>Kompyuta girindise</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="68"/>
         <source>Hibernate</source>
-        <translation>Hibernasi</translation>
+        <translation>Kompyuta gyebase</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="73"/>
         <source>Shut down</source>
-        <translation>Matikan</translation>
+        <translation>Gyako kompyuta</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="81"/>
         <location filename="../lidwatchersettings.ui" line="125"/>
         <source>On AC:</source>
-        <translation>Pada AC:</translation>
+        <translation>Nga eri ku masanyalaze:</translation>
     </message>
     <message>
         <location filename="../lidwatchersettings.ui" line="94"/>
         <source>When &amp;using external monitor</source>
-        <translation>Ketika mengunakan monitor eksternal</translation>
+        <translation>Nga ekozesa ol&amp;utimbe olwetongovu</translation>
     </message>
 </context>
 <context>
@@ -236,27 +236,27 @@
     <message>
         <location filename="../mainwindow.cpp" line="37"/>
         <source>Power Management Settings</source>
-        <translation>Pengaturan Manajemen Daya</translation>
+        <translation>Enteekateeka ez&apos;enkozesa ya muliro</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="40"/>
         <source>Battery</source>
-        <translation>Baterai</translation>
+        <translation>Lyanda</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Lid</source>
-        <translation>Penutup</translation>
+        <translation>Kisaanikira</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Idle</source>
-        <translation>Siaga</translation>
+        <translation>Kusiriikirira</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="52"/>
         <source>Power keys</source>
-        <translation>Tombol daya</translation>
+        <translation>Mapeesa agafuga enkola</translation>
     </message>
 </context>
 <context>
@@ -264,27 +264,27 @@
     <message>
         <location filename="../powerkeyssettings.ui" line="14"/>
         <source>Form</source>
-        <translation>Borang</translation>
+        <translation>Awajjuzibwa</translation>
     </message>
     <message>
         <location filename="../powerkeyssettings.ui" line="20"/>
         <source>Handle Power Keys</source>
-        <translation>Menangani Tombol Daya</translation>
+        <translation>Teekateeka amapeesa agafuga enkola</translation>
     </message>
     <message>
         <location filename="../powerkeyssettings.ui" line="26"/>
         <source>Power Key Action:</source>
-        <translation>Aksi Kunci Power:</translation>
+        <translation>Ppeesa ery&apos;omuliro kye likola:</translation>
     </message>
     <message>
         <location filename="../powerkeyssettings.ui" line="42"/>
         <source>Suspend Key Action:</source>
-        <translation type="unfinished">Menanggungkan tindakan:</translation>
+        <translation>Ppeesa erilindisa kye likola:</translation>
     </message>
     <message>
         <location filename="../powerkeyssettings.ui" line="49"/>
         <source>Hibernate Key Action:</source>
-        <translation>Aksi Kunci Hibernasi:</translation>
+        <translation>Ppeesa ery&apos;ebasa kye likola:</translation>
     </message>
 </context>
 <context>
@@ -292,37 +292,37 @@
     <message>
         <location filename="../helpers.cpp" line="35"/>
         <source>Nothing</source>
-        <translation>Tidak ada</translation>
+        <translation>Tewali</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="38"/>
         <source>Ask</source>
-        <translation>Tanya</translation>
+        <translation>Buuza</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="40"/>
         <source>Lock screen</source>
-        <translation>Kunci layar</translation>
+        <translation>Saanikira olutimbe</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="41"/>
         <source>Suspend</source>
-        <translation>Suspensi</translation>
+        <translation>Kompyuta girindise</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="42"/>
         <source>Hibernate</source>
-        <translation>Hibernasi</translation>
+        <translation>Kompyuta gyebase</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="43"/>
         <source>Shutdown</source>
-        <translation>Matikan</translation>
+        <translation>Gyako kompyuta</translation>
     </message>
     <message>
         <location filename="../helpers.cpp" line="44"/>
         <source>Turn Off monitor(s)</source>
-        <translation>Matikan monitor</translation>
+        <translation>Gyako ntimbe</translation>
     </message>
 </context>
 </TS>
