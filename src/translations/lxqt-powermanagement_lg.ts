5459baa8442a7553d44a8d1e49091a0fb38b010d
Weblate <noreply@weblate.org>
2024-02-19 09:23:22 +0100

    Weblate commits
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagement-module-name/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/de/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry
    Translation: LXQt Configuration/lxqt-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement Module Name

--- src/translations/lxqt-powermanagement_lg.ts
+++ src/translations/lxqt-powermanagement_lg.ts
@@ -1,140 +1,138 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru">
+<TS version="2.1" language="lg">
 <context>
     <name>BatteryHelper</name>
     <message>
         <location filename="../batteryhelper.cpp" line="44"/>
         <source>Stable</source>
-        <translation>Стабильный</translation>
+        <translation>Ddamu</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="46"/>
         <source>Discharging</source>
-        <translation>Разряжается</translation>
+        <translation>Linywebwa</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="48"/>
         <source>Fully charged</source>
-        <translation>Полностью заряжена</translation>
+        <translation>Lijjudde</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="51"/>
         <source>Charging</source>
-        <translation>Заряжается</translation>
+        <translation>Linywa</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="60"/>
         <source>Lithium ion</source>
-        <translation>Литий-ионная</translation>
+        <translation>Lya &apos;lithium ion&apos;</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="62"/>
         <source>Lithium polymer</source>
-        <translation>Литий-полимерная</translation>
+        <translation>Lya &apos;Lithium polymer&apos;</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="64"/>
         <source>Lithium iron phosphate</source>
-        <translation>Литий-железо-фосфатная</translation>
+        <translation>Lya &apos;Lithium iron phosphate&apos;</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="66"/>
         <source>Lead acid</source>
-        <translation>Свинцово-кислотная</translation>
+        <translation>Lya &apos;Lead acid&apos;</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="68"/>
         <source>Nickel cadmium</source>
-        <translation>Никель-кадмиевая</translation>
+        <translation>Lya &apos;Nickel cadmium&apos;</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="70"/>
         <source>Nickel metal hydride</source>
-        <translation>Никель-метал-гидридная</translation>
+        <translation>Lya &apos;Nickel metal hydride&apos;</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="73"/>
         <source>Unknown</source>
-        <translation>Неизвестно</translation>
+        <translation>Lya kika ekitategeerese</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="82"/>
         <source>Personal Digital Assistant&apos;s battery</source>
-        <translation>Батарея карманного персонального компьютера</translation>
+        <translation>Lyand lya PDA</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="84"/>
         <source>Uninterruptible Power Supply&apos;s battery</source>
-        <translation>Батарея источника бесперебойного электропитания</translation>
+        <translation>Lyanda zzibizi</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="86"/>
         <source>Primary battery</source>
-        <translation>Основная батарея</translation>
+        <translation>Lyanda ery&apos;ennono</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="88"/>
         <source>Mouse battery</source>
-        <translation>Батарея мыши</translation>
+        <translation>Lyanda ly&apos;akasongesebwa</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="90"/>
         <source>Keyboard battery</source>
-        <translation>Батарея клавиатуры</translation>
+        <translation>Lyanda ly&apos;amapeesa agawandisibwa</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="92"/>
         <source>Keyboard and mouse&apos;s battery</source>
-        <translation>Батарея мыши и клавиатуры</translation>
+        <translation>Lyanda ly&apos;amapeesa agawandisibwa n&apos;akasongesebwa</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="94"/>
         <source>Camera battery</source>
-        <translation>Батарея камеры</translation>
+        <translation>Lyanda lya kamera</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="96"/>
         <source>Phone battery</source>
-        <translation>Батарея телефона</translation>
+        <translation>Lyanda lya ssimu</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="98"/>
         <source>Monitor battery</source>
-        <translation>Батарея монитора</translation>
+        <translation>Lyanda lya lutimbe</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="101"/>
         <source>Unknown battery</source>
-        <translation>Неизвестная батарея</translation>
+        <translation>Lyanda eritategeeese</translation>
     </message>
     <message numerus="yes">
         <location filename="../batteryhelper.cpp" line="111"/>
         <source>%Ln hour(s)</source>
         <translation>
-            <numerusform>%Ln час</numerusform>
-            <numerusform>%Ln часа</numerusform>
-            <numerusform>%Ln часов</numerusform>
+            <numerusform>Essaawa %Ln</numerusform>
+            <numerusform>Saawa %Ln</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../batteryhelper.cpp" line="112"/>
         <source>%Ln minute(s)</source>
         <translation>
-            <numerusform>%Ln минута</numerusform>
-            <numerusform>%Ln минуты</numerusform>
-            <numerusform>%Ln минут</numerusform>
+            <numerusform>Eddakiika %Ln</numerusform>
+            <numerusform>Ddakiika %Ln</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="117"/>
         <source>remaining %1</source>
-        <translation>осталось %1</translation>
+        <translation>wasigadde %1</translation>
     </message>
     <message>
         <location filename="../batteryhelper.cpp" line="122"/>
         <source>to full %1</source>
-        <translation>до полной зарядки %1</translation>
+        <translation>%1 eryanda okujjula</translation>
     </message>
 </context>
 <context>
@@ -142,12 +140,12 @@
     <message>
         <location filename="../batteryinfodialog.ui" line="14"/>
         <source>Dialog</source>
-        <translation>Диалог</translation>
+        <translation>Kaboozi</translation>
     </message>
     <message>
         <location filename="../batteryinfodialog.cpp" line="24"/>
         <source>Battery Info</source>
-        <translation>Информация о батарее</translation>
+        <translation>Ebifa ku lyanda</translation>
     </message>
 </context>
 <context>
@@ -155,62 +153,62 @@
     <message>
         <location filename="../batteryinfoframe.ui" line="14"/>
         <source>Power Management</source>
-        <translation>Управление энергопотреблением</translation>
+        <translation>Enteekateeka z&apos;enkozesa y&apos;amasanyalaze</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="53"/>
         <source>Temperature:</source>
-        <translation>Температура:</translation>
+        <translation>Bbugumu:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="63"/>
         <source>Type:</source>
-        <translation>Тип:</translation>
+        <translation>Kika:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="73"/>
         <source>State:</source>
-        <translation>Состояние:</translation>
+        <translation>Embeera:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="90"/>
         <source>Energy Full Design:</source>
-        <translation>Максимальный заряд:</translation>
+        <translation>Omuliro kwe lyakolebwa okujjulira:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="107"/>
         <source>Energy Full:</source>
-        <translation>Полный заряд:</translation>
+        <translation>Omuliro kwe lijjulidde:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="124"/>
         <source>Energy Now:</source>
-        <translation>Текущий заряд:</translation>
+        <translation>Omuliro kakano:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="141"/>
         <source>Energy rate:</source>
-        <translation>Мощность тока:</translation>
+        <translation>Okukyuka kw&apos;omuliro:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="165"/>
         <source>Technology:</source>
-        <translation>Технология:</translation>
+        <translation>Tekinologiya:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="182"/>
         <source>Voltage:</source>
-        <translation>Напряжение:</translation>
+        <translation>Voloti:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.ui" line="192"/>
         <source>Vendor:</source>
-        <translation>Производитель:</translation>
+        <translation>Abaalikola:</translation>
     </message>
     <message>
         <location filename="../batteryinfoframe.cpp" line="65"/>
         <source>Empty</source>
-        <translation>Разряжена</translation>
+        <translation>Kkalu</translation>
     </message>
 </context>
 <context>
@@ -218,43 +216,40 @@
     <message>
         <location filename="../batterywatcher.cpp" line="48"/>
         <source>No battery!</source>
-        <translation>Нет батареи!</translation>
+        <translation>Temuli lyanda!</translation>
     </message>
     <message>
         <location filename="../batterywatcher.cpp" line="49"/>
         <source>LXQt could not find data about any battery - monitoring disabled</source>
-        <translation>LXQt не смог найти информацию о какой-либо батарее - наблюдение отменено</translation>
+        <translation>LXQt tezuddeyo ebikwata ku manda - tejja okugeza okugalabiriza</translation>
     </message>
     <message>
         <location filename="../batterywatcher.cpp" line="119"/>
         <source>Power low!</source>
-        <translation>Низкий заряд!</translation>
+        <translation>Omuliro mutono!</translation>
     </message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="131"/>
         <source>Suspending in %Ln second(s)</source>
         <translation>
-            <numerusform>Ждущий режим через %Ln секунду</numerusform>
-            <numerusform>Ждущий режим через %Ln секунды</numerusform>
-            <numerusform>Ждущий режим через %Ln секунд</numerusform>
+            <numerusform>Ebulayo sikonda %Ln kompyuta erindisibwe</numerusform>
+            <numerusform>Wasigadde sikonda %Ln kompyuta erindisibwe</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="134"/>
         <source>Hibernating in %Ln second(s)</source>
         <translation>
-            <numerusform>Спящий режим через %Ln секунду</numerusform>
-            <numerusform>Спящий режим через %Ln секунды</numerusform>
-            <numerusform>Спящий режим через %Ln секунд</numerusform>
+            <numerusform>Ebulayo sikonda %Ln kompyuta yebasibwe</numerusform>
+            <numerusform>Wasigadde sikonda %Ln kompyuta yebasibwe</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../batterywatcher.cpp" line="137"/>
         <source>Shutting down in %Ln second(s)</source>
         <translation>
-            <numerusform>Выключение через %Ln секунду</numerusform>
-            <numerusform>Выключение через %Ln секунды</numerusform>
-            <numerusform>Выключение через %Ln секунд</numerusform>
+            <numerusform>Ebulayo sikonda %Ln kompyuta eveeko</numerusform>
+            <numerusform>Wasigadde sikonda %Ln kompyuta eveeko</numerusform>
         </translation>
     </message>
 </context>
@@ -263,17 +258,17 @@
     <message>
         <location filename="../powerbutton.cpp" line="72"/>
         <source>Power off key action</source>
-        <translation>Действие кнопки питания</translation>
+        <translation>Ekibawo ng&apos;onyize eppeesa ery&apos;omuliro</translation>
     </message>
     <message>
         <location filename="../powerbutton.cpp" line="79"/>
         <source>Suspend key action</source>
-        <translation>Действие кнопки сна</translation>
+        <translation>Ekibawo ng&apos;onyize eppeesa eririndisa</translation>
     </message>
     <message>
         <location filename="../powerbutton.cpp" line="86"/>
         <source>Hibernate key action</source>
-        <translation>Действие кнопки «Спящий режим»</translation>
+        <translation>Ekibawo ng&apos;onyize eppeesa ery&apos;ebasa</translation>
     </message>
 </context>
 <context>
@@ -281,93 +276,94 @@
     <message>
         <location filename="../powermanagementd.cpp" line="115"/>
         <source>Power Management</source>
-        <translation>Управление энергопотреблением</translation>
+        <translation>Enteekateeka z&apos;enkozesa y&apos;amasanyalaze</translation>
     </message>
     <message>
         <location filename="../powermanagementd.cpp" line="116"/>
         <source>You are running LXQt Power Management for the first time.
 You can configure it from settings... </source>
-        <translation>Вы запускаете управление энергопотреблением LXQt впервые.
-Вы можете изменить конфигурацию в настройках… </translation>
+        <translation>Guno gwe gusoose okutandika enteekateeka z&apos;enkozesa y&apos;amasanyalaze eza mu LXQt.
+Osobola okugisanga ne mu ntabiro y&apos;enteekateeka... </translation>
     </message>
     <message>
         <location filename="../powermanagementd.cpp" line="117"/>
         <source>Configure...</source>
-        <translation>Настроить…</translation>
+        <translation>Teekateeka...</translation>
     </message>
 </context>
 <context>
     <name>TrayIcon</name>
     <message>
-        <location filename="../trayicon.cpp" line="101"/>
-        <location filename="../trayicon.cpp" line="193"/>
-        <source>About</source>
-        <translation>О программе</translation>
-    </message>
-    <message>
         <location filename="../trayicon.cpp" line="67"/>
         <source>Configure</source>
-        <translation>Настроить</translation>
+        <translation>Teekateeka</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="75"/>
         <source>30 minutes</source>
-        <translation>30 минут</translation>
+        <translation>Ddakiika 30</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="79"/>
         <source>1 hour</source>
-        <translation>1 час</translation>
+        <translation>Saawa 1</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="83"/>
         <source>2 hours</source>
-        <translation>2 часа</translation>
+        <translation>Saawa 2</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="87"/>
         <source>3 hours</source>
-        <translation>3 часа</translation>
+        <translation>Saawa 3</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="91"/>
         <source>4 hours</source>
-        <translation>4 часа</translation>
+        <translation>Saawa 4</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="96"/>
         <source>Pause idleness checks</source>
-        <translation>Приостановить проверку бездействия</translation>
+        <translation>Yimirizamu okulabiriza busiriikiriro</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="101"/>
+        <location filename="../trayicon.cpp" line="193"/>
+        <source>About</source>
+        <translation>Ebifa ku puloguramu</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="103"/>
         <source>Disable icon</source>
-        <translation>Не показывать значок</translation>
+        <translation>Gyawo kafaananyi</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="162"/>
         <source>Empty</source>
-        <translation>Разряжена</translation>
+        <translation>Kkalu</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="194"/>
         <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
-        <translation>&lt;p&gt;  &lt;b&gt;Управление энергопотреблением LXQt&lt;/b&gt;&lt;br/&gt;  - Управление энергопотреблением для окружения рабочего стола LXQt&lt;/p&gt;&lt;p&gt; Авторы:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - и другие из проектов Razor и LXQt&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</translation>
+        <translation>&lt;p&gt;  &lt;b&gt;Enteekateeka z&apos;enkozesa ya masanyalaze eza mu LXQt&lt;/b&gt;&lt;br/&gt;  - W&apos;oteekateekera awakolerwa wa LXQt eby&apos;enkozesa y&apos;amasanyalaze&lt;/p&gt;&lt;p&gt;  Abaawandika:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - n&apos;abalala ab&apos;enyigide mu kawefube wa Razor ne LXQt &lt;/p&gt;&lt;p&gt;  Buyinza bwa kukopporola &amp;copy; 2012-2014&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="212"/>
         <source>LXQt Power Management info</source>
-        <translation>Сведения об управлении энергопотреблением LXQt</translation>
+        <translation>Ebikwata ku nteekateeka z&apos;enkozesa ya masanyalaze eza mu LXQt</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="213"/>
         <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
-        <translation>Значок в системном лотке для управление энергопотреблением может быть включён в &lt;i&gt;Настройках управления энергопотреблением&lt;/i&gt;</translation>
+        <translation>Bw&apos;ogenda mu &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt; waliwo ekikomyawo akafaananyi
+ak&apos;omu kakuŋaanizo ak&apos;enteekateeka z&apos;enkozesa ya masanyalaze mu LXQt</translation>
     </message>
     <message>
         <location filename="../trayicon.cpp" line="215"/>
         <source>Configure now</source>
-        <translation>Настроить сейчас</translation>
+        <translation>Teekateeka kakati</translation>
     </message>
 </context>
 </TS>
