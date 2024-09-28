a9fbb84256efde32e665d4f4b368e44da65516db
Weblate <noreply@weblate.org>
2024-09-28 17:24:42 +0200

    Weblate commits
    Co-authored-by: Davidmp <opensusecatala@gmail.com>
    Co-authored-by: Deleted User <noreply+435@weblate.org>
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/zh_CN/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry

--- config/translations/lxqt-config-powermanagement_ca.ts
+++ config/translations/lxqt-config-powermanagement_ca.ts
@@ -289,7 +289,7 @@
     <message>
         <location filename="../powerkeyssettings.cpp" line="48"/>
         <source>Note: Under Wayland, power keys are configured by the compositor instead.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nota: amb Wayland, les tecles d&apos;engegada les configura el compositor.</translation>
     </message>
 </context>
 <context>
