77f7c8738493fb15a73fef3c7d2f2db4cc3a00b4
Standreas <standreas@riseup.net>
2024-10-23 15:50:39 +0200

    Updated translations (incl. Luganda) (#413)

--- config/translations/lxqt-config-powermanagement_es.ts
+++ config/translations/lxqt-config-powermanagement_es.ts
@@ -102,7 +102,7 @@
     <message>
         <location filename="../batterywatchersettings.cpp" line="109"/>
         <source>Level: %1%</source>
-        <translation>Nivel: %1 %</translation>
+        <translation>Nivel: %1&#xa0;%</translation>
     </message>
 </context>
 <context>
