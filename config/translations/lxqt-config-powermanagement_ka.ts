db068f48fcc79669c0955b0fa85faa4338b38cfd
Weblate <noreply@weblate.org>
2025-01-02 14:25:19 +0100

    Weblate commits
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagement-module-name/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/ka/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry
    Translation: LXQt Configuration/lxqt-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement Module Name

---
+++ config/translations/lxqt-config-powermanagement_ka.ts
@@ -0,0 +1,333 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>BatteryWatcherSettings</name>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="14"/>
+        <source>Battery Watcher Settings</source>
+        <translation>ელემენტის მონიტორინგის მორგება</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="26"/>
+        <source>E&amp;nable Battery Watcher</source>
+        <translation>ელემე&amp;ნტის მონიტორინგის ჩართვა</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="54"/>
+        <source>Warning:</source>
+        <translation>გაფრთხილება:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="67"/>
+        <source> seconds</source>
+        <translation> წამი</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="77"/>
+        <source>Level is low at:</source>
+        <translation>დონე დაბალია:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="90"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="111"/>
+        <source>Icon:</source>
+        <translation>ხატულა:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="121"/>
+        <location filename="../batterywatchersettings.cpp" line="104"/>
+        <source>Preview</source>
+        <translation>მინიატურა</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="158"/>
+        <source>Charging</source>
+        <translation>იტენება</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="196"/>
+        <source>Discharging</source>
+        <translation>იცლება</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="41"/>
+        <source>When power is low then:</source>
+        <translation>რა მოხდება, როცა მუხტი დაბალია:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.ui" line="97"/>
+        <source>Show icon:</source>
+        <translation>ხატულას ჩვენება:</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="41"/>
+        <source>from theme</source>
+        <comment>icons</comment>
+        <translation>თემიდან</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="42"/>
+        <source>Circle</source>
+        <comment>icons</comment>
+        <translation>წრე</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="43"/>
+        <source>Circle with percentage</source>
+        <comment>icons</comment>
+        <translation>წრე პროცენტებით</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="44"/>
+        <source>Battery with percentage and background</source>
+        <comment>icons</comment>
+        <translation>ელემენტი პროცენტებით და ფონი</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="45"/>
+        <source>Battery with percentage</source>
+        <comment>icons</comment>
+        <translation>ელემენტი პროცენტებით</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="104"/>
+        <source>built in</source>
+        <translation>ჩაშენებული</translation>
+    </message>
+    <message>
+        <location filename="../batterywatchersettings.cpp" line="109"/>
+        <source>Level: %1%</source>
+        <translation>დონე: %1%</translation>
+    </message>
+</context>
+<context>
+    <name>IdlenessWatcherSettings</name>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="17"/>
+        <source>Enab&amp;le Idleness Watcher</source>
+        <translation>უქმეობის მონიტორინ&amp;გის ჩართვა</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="151"/>
+        <source>Idle time:</source>
+        <translation>უქმეობის დრო:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="122"/>
+        <source>Enable Backlight Change</source>
+        <translation>უკუგანათების ცვლილების ჩართვა</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="83"/>
+        <source>When idle on Battery:</source>
+        <translation>როცა უქმეა, ელემენტზე:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="90"/>
+        <source>Battery Idle time:</source>
+        <translation>ელემენტის უქმეობის დრო:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="131"/>
+        <source>Backlight (%):</source>
+        <translation>უკუგანათება (%):</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="158"/>
+        <source>Minutes:Seconds</source>
+        <translation>წთ:წმ</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="76"/>
+        <location filename="../idlenesswatchersettings.ui" line="110"/>
+        <location filename="../idlenesswatchersettings.ui" line="178"/>
+        <source>mm:ss</source>
+        <translation>წთ:წმ</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="34"/>
+        <source>When idle on AC:</source>
+        <translation>როცა უქმეა კვებით:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="53"/>
+        <source>AC Idle time:</source>
+        <translation>შეერთებული უქმეობის დრო:</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="63"/>
+        <location filename="../idlenesswatchersettings.ui" line="97"/>
+        <source>Minutes:Seconds (min: 01:30)</source>
+        <translation>წუთი:წამი (წუთი:01:30)</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="185"/>
+        <source>Check backlight</source>
+        <translation>უკუგანათების შემოწმება</translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="192"/>
+        <source>On battery discharging </source>
+        <translation>მუშაობს ელემენტზე </translation>
+    </message>
+    <message>
+        <location filename="../idlenesswatchersettings.ui" line="208"/>
+        <source>Disable idleness checks when fullscreen</source>
+        <translation>უქმეობის შემოწმების გათიშვა სრულ ეკრანზე</translation>
+    </message>
+</context>
+<context>
+    <name>LidWatcherSettings</name>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="23"/>
+        <source>Enable &amp;Lid Watcher</source>
+        <translation>ნოუთბუქის დახურვის მონიტორინგის ჩართვ&amp;ა</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="38"/>
+        <source>Action when lid is closed:</source>
+        <translation>ქმედება, როცა ნოუთბუქს დახურავენ:</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="50"/>
+        <location filename="../lidwatchersettings.ui" line="115"/>
+        <source>On Battery:</source>
+        <translation>ელემენტზე:</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="58"/>
+        <source>Nothing</source>
+        <translation>არაფერი</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="63"/>
+        <source>Suspend</source>
+        <translation>ძილი</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="68"/>
+        <source>Hibernate</source>
+        <translation>პროგრამული ძილი</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="73"/>
+        <source>Shut down</source>
+        <translation>გამორთვა</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="81"/>
+        <location filename="../lidwatchersettings.ui" line="125"/>
+        <source>On AC:</source>
+        <translation>შეერთებული:</translation>
+    </message>
+    <message>
+        <location filename="../lidwatchersettings.ui" line="94"/>
+        <source>When &amp;using external monitor</source>
+        <translation>&amp;გარე ეკრანის გამოყენებისას</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Power Management Settings</source>
+        <translation>კვების მართვის მორგება</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="40"/>
+        <source>Battery</source>
+        <translation>ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <source>Lid</source>
+        <translation>თავსახური</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="48"/>
+        <source>Idle</source>
+        <translation>უმოქმედო</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="52"/>
+        <source>Power keys</source>
+        <translation>კვების ღილაკები</translation>
+    </message>
+</context>
+<context>
+    <name>PowerKeysSettings</name>
+    <message>
+        <location filename="../powerkeyssettings.ui" line="14"/>
+        <source>Form</source>
+        <translation>ფორმა</translation>
+    </message>
+    <message>
+        <location filename="../powerkeyssettings.ui" line="20"/>
+        <source>Handle Power Keys</source>
+        <translation>კვების ღილაკების მართვა</translation>
+    </message>
+    <message>
+        <location filename="../powerkeyssettings.ui" line="26"/>
+        <source>Power Key Action:</source>
+        <translation>კვების ღილაკის ქმედება:</translation>
+    </message>
+    <message>
+        <location filename="../powerkeyssettings.ui" line="42"/>
+        <source>Suspend Key Action:</source>
+        <translation>შეჩერების ღილაკის ქმედება:</translation>
+    </message>
+    <message>
+        <location filename="../powerkeyssettings.ui" line="49"/>
+        <source>Hibernate Key Action:</source>
+        <translation>პროგრამული ძილის ღილაკის ქმედება:</translation>
+    </message>
+    <message>
+        <location filename="../powerkeyssettings.cpp" line="48"/>
+        <source>Note: Under Wayland, power keys are configured by the compositor instead.</source>
+        <translation>შენიშვნა: Wayland-ში კვების ღილაკებს კომპოზიტორი მართავს.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../helpers.cpp" line="35"/>
+        <source>Nothing</source>
+        <translation>არაფერი</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="38"/>
+        <source>Ask</source>
+        <translation>კითხვა</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="40"/>
+        <source>Lock screen</source>
+        <translation>ეკრანის დაბლოკვა</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="41"/>
+        <source>Suspend</source>
+        <translation>ძილი</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="42"/>
+        <source>Hibernate</source>
+        <translation>პროგრამული ძილი</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="43"/>
+        <source>Shutdown</source>
+        <translation>გამორთვა</translation>
+    </message>
+    <message>
+        <location filename="../helpers.cpp" line="44"/>
+        <source>Turn Off monitor(s)</source>
+        <translation>ეკრან(ებ)-ის გამორთვა</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/lxqt-powermanagement_ka.ts
@@ -0,0 +1,368 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>BatteryHelper</name>
+    <message>
+        <location filename="../batteryhelper.cpp" line="44"/>
+        <source>Stable</source>
+        <translation>სტაბილური</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="46"/>
+        <source>Discharging</source>
+        <translation>იცლება</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="48"/>
+        <source>Fully charged</source>
+        <translation>სრულად დატენილი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="51"/>
+        <source>Charging</source>
+        <translation>იტენება</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="60"/>
+        <source>Lithium ion</source>
+        <translation>ლითიუმ-იონი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="62"/>
+        <source>Lithium polymer</source>
+        <translation>ლითიუმ-პოლიმერი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="64"/>
+        <source>Lithium iron phosphate</source>
+        <translation>ლითიუმ-რკინის ფოსფატი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="66"/>
+        <source>Lead acid</source>
+        <translation>ტყვია-მჟავა</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="68"/>
+        <source>Nickel cadmium</source>
+        <translation>ნიკელ კადმიუმი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="70"/>
+        <source>Nickel metal hydride</source>
+        <translation>ნიკელი-ლითონის-ჰიდრიდი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="73"/>
+        <source>Unknown</source>
+        <translation>უცნობი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="82"/>
+        <source>Personal Digital Assistant&apos;s battery</source>
+        <translation>პირადი ციფრული დამხმარის ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="84"/>
+        <source>Uninterruptible Power Supply&apos;s battery</source>
+        <translation>უწყვეტი კვების წყაროს ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="86"/>
+        <source>Primary battery</source>
+        <translation>ძირითადი ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="88"/>
+        <source>Mouse battery</source>
+        <translation>თაგუნას ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="90"/>
+        <source>Keyboard battery</source>
+        <translation>კლავიატურის ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="92"/>
+        <source>Keyboard and mouse&apos;s battery</source>
+        <translation>კლავიატურის და თაგუნას ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="94"/>
+        <source>Camera battery</source>
+        <translation>კამერის ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="96"/>
+        <source>Phone battery</source>
+        <translation>ტელეფონის ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="98"/>
+        <source>Monitor battery</source>
+        <translation>ეკრანის ელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="101"/>
+        <source>Unknown battery</source>
+        <translation>უცნობი ელემენტი</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batteryhelper.cpp" line="111"/>
+        <source>%Ln hour(s)</source>
+        <translation>
+            <numerusform>%Ln საათი</numerusform>
+            <numerusform>%Ln საათი</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batteryhelper.cpp" line="112"/>
+        <source>%Ln minute(s)</source>
+        <translation>
+            <numerusform>%Ln წუთი</numerusform>
+            <numerusform>%Ln წუთი</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="117"/>
+        <source>remaining %1</source>
+        <translation>დარჩენილია %1</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="122"/>
+        <source>to full %1</source>
+        <translation>ავსებამდე %1</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoDialog</name>
+    <message>
+        <location filename="../batteryinfodialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>ფანჯარა</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfodialog.cpp" line="24"/>
+        <source>Battery Info</source>
+        <translation>ელემენტის ინფორმაცია</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoFrame</name>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>კვების მართვა</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="53"/>
+        <source>Temperature:</source>
+        <translation>ტემპერატურა:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="63"/>
+        <source>Type:</source>
+        <translation>ტიპი:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="73"/>
+        <source>State:</source>
+        <translation>მდგომარეობა:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="90"/>
+        <source>Energy Full Design:</source>
+        <translation>ენერგიის სრული დიზაინი:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="107"/>
+        <source>Energy Full:</source>
+        <translation>ენერგია სრულია:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="124"/>
+        <source>Energy Now:</source>
+        <translation>ენერგია ახლა:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="141"/>
+        <source>Energy rate:</source>
+        <translation>ენერგიის სიხშირე:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="165"/>
+        <source>Technology:</source>
+        <translation>ტექნოლოგია:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="182"/>
+        <source>Voltage:</source>
+        <translation>ვოლტაჟი:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="192"/>
+        <source>Vendor:</source>
+        <translation>მომწოდებელი:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.cpp" line="65"/>
+        <source>Empty</source>
+        <translation>ცარიელი</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="48"/>
+        <source>No battery!</source>
+        <translation>ელემენტის გარეშე!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQt-მა ელემენტების შესახებ ინფორმაცია ვერ მოიპოვა - მონიტორინგი გათიშულია</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="119"/>
+        <source>Power low!</source>
+        <translation>მუხტი ცოტაღაა!</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="131"/>
+        <source>Suspending in %Ln second(s)</source>
+        <translation>
+            <numerusform>ძილი %Ln წამში</numerusform>
+            <numerusform>ძილი %Ln წამში</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="134"/>
+        <source>Hibernating in %Ln second(s)</source>
+        <translation>
+            <numerusform>პროგრამული ძილი %Ln წამში</numerusform>
+            <numerusform>პროგრამული ძილი %Ln წამში</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="137"/>
+        <source>Shutting down in %Ln second(s)</source>
+        <translation>
+            <numerusform>გათიშვა %Ln წამში</numerusform>
+            <numerusform>გათიშვა %Ln წამში</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PowerButton</name>
+    <message>
+        <location filename="../powerbutton.cpp" line="72"/>
+        <source>Power off key action</source>
+        <translation>გამორთვის ღილაკის ქმედება</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="79"/>
+        <source>Suspend key action</source>
+        <translation>ძილის ღილაკის ქმედება</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="86"/>
+        <source>Hibernate key action</source>
+        <translation>პროგრამული ძილის ღილაკის ქმედება</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="115"/>
+        <source>Power Management</source>
+        <translation>კვების მართვა</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="116"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>LXQt-ის კვების მართვა პირველად გაუშვით.
+მისი მორგება პარამეტრების დიალოგიდან შეგიძლიათ... </translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="117"/>
+        <source>Configure...</source>
+        <translation>გამართვა...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="68"/>
+        <source>Configure</source>
+        <translation>მორგება</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="76"/>
+        <source>30 minutes</source>
+        <translation>30 წუთი</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="80"/>
+        <source>1 hour</source>
+        <translation>1 საათი</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="84"/>
+        <source>2 hours</source>
+        <translation>2 საათი</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="88"/>
+        <source>3 hours</source>
+        <translation>3 საათი</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="92"/>
+        <source>4 hours</source>
+        <translation>4 საათი</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="97"/>
+        <source>Pause idleness checks</source>
+        <translation>უქმეობის შემოწმებების შეჩერება</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="102"/>
+        <location filename="../trayicon.cpp" line="194"/>
+        <source>About</source>
+        <translation>შესახებ</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="104"/>
+        <source>Disable icon</source>
+        <translation>ხატულას გამორთვა</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="163"/>
+        <source>Empty</source>
+        <translation>ცარიელი</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="195"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2014&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt-ის კვების მართვა&lt;/b&gt;&lt;br/&gt;  - კვების მართვა სამუშაო მაგიდისთვის LXQt&lt;/p&gt;&lt;p&gt;  ავტორები:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - და სხვები პროექტებიდან Razor და LXQt&lt;/p&gt;&lt;p&gt;  საავტორო უფლებები დაცულია &amp;copy; 2012-2014&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="213"/>
+        <source>LXQt Power Management info</source>
+        <translation>LXQt-ის კვების მართვის ინფორმაცია</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="214"/>
+        <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
+        <translation>LXQt-ის კვების მართვის სისტემური კუთხის ხატულის ჩართვა შეგიძლიათ ბრძანებით &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="216"/>
+        <source>Configure now</source>
+        <translation>მორგება ახლავე</translation>
+    </message>
+</context>
+</TS>
