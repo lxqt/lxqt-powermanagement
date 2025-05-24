f9d0eccaed7028c30915b9594ab5c3c4593a8911
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-05-24 22:30:32 +0200

    Weblate commits (#434)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/uk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/vi/
    Translation: LXQt Configuration/lxqt-powermanagement
    
    Co-authored-by: Ettore Atalan <atalanttore@googlemail.com>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Trung Do <dothanhtrung@pm.me>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Co-authored-by: opensourcemount <opensourcemount@gmail.com>

--- src/translations/lxqt-powermanagement_de.ts
+++ src/translations/lxqt-powermanagement_de.ts
@@ -259,24 +259,24 @@
         <location filename="../PowerProfiles.cpp" line="38"/>
         <source>power-saver</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>Energiesparen</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="39"/>
         <source>balanced</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>Ausgeglichen</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="40"/>
         <source>performance</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>Leistung</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="69"/>
         <source>Power profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Energieprofil</translation>
     </message>
 </context>
 <context>
