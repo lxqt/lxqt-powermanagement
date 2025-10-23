449319b872cf0ee04870473c3db277b83f98b5f3
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-10-23 15:21:58 +0200

    Weblate commits (#457)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/es/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement
    
    Co-authored-by: David Medina <dmedinacpnl@gmail.com>
    Co-authored-by: Davidmp <opensusecatala@gmail.com>
    Co-authored-by: Megido <hibarioath@proton.me>

--- src/translations/lxqt-powermanagement_ca.ts
+++ src/translations/lxqt-powermanagement_ca.ts
@@ -259,24 +259,24 @@
         <location filename="../PowerProfiles.cpp" line="38"/>
         <source>power-saver</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>estalvi d&apos;energia</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="39"/>
         <source>balanced</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>equilibrat</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="40"/>
         <source>performance</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>rendiment</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="69"/>
         <source>Power profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Perfil d&apos;energia</translation>
     </message>
 </context>
 <context>
