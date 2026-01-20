979ee300a5e5c7b458a81acf23d9d7ed07c28acc
Weblate <noreply@weblate.org>
2026-01-20 17:28:16 +0100

    Weblate commits
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-powermanagement-menu-entry/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagement-module-name/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/kk/
    Translation: LXQt Configuration/lxqt-config-powermanagement Menu Entry
    Translation: LXQt Configuration/lxqt-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement Module Name

---
+++ src/translations/lxqt-powermanagement_kk.ts
@@ -0,0 +1,394 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kk">
+<context>
+    <name>BatteryHelper</name>
+    <message>
+        <location filename="../batteryhelper.cpp" line="44"/>
+        <source>Stable</source>
+        <translation>Тұрақты</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="46"/>
+        <source>Discharging</source>
+        <translation>Заряд беруде</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="48"/>
+        <source>Fully charged</source>
+        <translation>Толық зарядталған</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="51"/>
+        <source>Charging</source>
+        <translation>Зарядталуда</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="60"/>
+        <source>Lithium ion</source>
+        <translation>Литий-ионды</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="62"/>
+        <source>Lithium polymer</source>
+        <translation>Литий-полимерлі</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="64"/>
+        <source>Lithium iron phosphate</source>
+        <translation>Литий-темір-фосфатты</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="66"/>
+        <source>Lead acid</source>
+        <translation>Қорғасын-қышқылды</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="68"/>
+        <source>Nickel cadmium</source>
+        <translation>Никель-кадмийлі</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="70"/>
+        <source>Nickel metal hydride</source>
+        <translation>Никель-металл-гидридті</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="73"/>
+        <source>Unknown</source>
+        <translation>Белгісіз</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="82"/>
+        <source>Personal Digital Assistant&apos;s battery</source>
+        <translation>Жеке сандық көмекшінің батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="84"/>
+        <source>Uninterruptible Power Supply&apos;s battery</source>
+        <translation>Үздіксіз қуат көзінің батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="86"/>
+        <source>Primary battery</source>
+        <translation>Негізгі батарея</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="88"/>
+        <source>Mouse battery</source>
+        <translation>Тышқан батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="90"/>
+        <source>Keyboard battery</source>
+        <translation>Пернетақта батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="92"/>
+        <source>Keyboard and mouse&apos;s battery</source>
+        <translation>Пернетақта мен тышқан батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="94"/>
+        <source>Camera battery</source>
+        <translation>Камера батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="96"/>
+        <source>Phone battery</source>
+        <translation>Телефон батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="98"/>
+        <source>Monitor battery</source>
+        <translation>Монитор батареясы</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="101"/>
+        <source>Unknown battery</source>
+        <translation>Белгісіз батарея</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batteryhelper.cpp" line="111"/>
+        <source>%Ln hour(s)</source>
+        <translation>
+            <numerusform>%Ln сағат</numerusform>
+            <numerusform>%Ln сағат</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batteryhelper.cpp" line="112"/>
+        <source>%Ln minute(s)</source>
+        <translation>
+            <numerusform>%Ln минут</numerusform>
+            <numerusform>%Ln минут</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="117"/>
+        <source>remaining %1</source>
+        <translation>%1 қалды</translation>
+    </message>
+    <message>
+        <location filename="../batteryhelper.cpp" line="122"/>
+        <source>to full %1</source>
+        <translation>толық болғанша %1</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoDialog</name>
+    <message>
+        <location filename="../batteryinfodialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Сұхбат терезесі</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfodialog.cpp" line="24"/>
+        <source>Battery Info</source>
+        <translation>Батарея ақпараты</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryInfoFrame</name>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="14"/>
+        <source>Power Management</source>
+        <translation>Қуатты басқару</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="53"/>
+        <source>Temperature:</source>
+        <translation>Температура:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="63"/>
+        <source>Type:</source>
+        <translation>Түрі:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="73"/>
+        <source>State:</source>
+        <translation>Күйі:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="90"/>
+        <source>Energy Full Design:</source>
+        <translation>Толық сыйымдылығы (жоспарланған):</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="107"/>
+        <source>Energy Full:</source>
+        <translation>Толық сыйымдылығы:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="124"/>
+        <source>Energy Now:</source>
+        <translation>Ағымдағы қуаты:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="141"/>
+        <source>Energy rate:</source>
+        <translation>Қуат деңгейі:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="165"/>
+        <source>Technology:</source>
+        <translation>Технологиясы:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="182"/>
+        <source>Voltage:</source>
+        <translation>Кернеуі:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.ui" line="192"/>
+        <source>Vendor:</source>
+        <translation>Өндіруші:</translation>
+    </message>
+    <message>
+        <location filename="../batteryinfoframe.cpp" line="65"/>
+        <source>Empty</source>
+        <translation>Бос</translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWatcher</name>
+    <message>
+        <location filename="../batterywatcher.cpp" line="48"/>
+        <source>No battery!</source>
+        <translation>Батарея жоқ!</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="49"/>
+        <source>LXQt could not find data about any battery - monitoring disabled</source>
+        <translation>LXQt ешқандай батарея туралы дерек таба алмады - бақылау сөндірілген</translation>
+    </message>
+    <message>
+        <location filename="../batterywatcher.cpp" line="119"/>
+        <source>Power low!</source>
+        <translation>Қуат деңгейі төмен!</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="131"/>
+        <source>Suspending in %Ln second(s)</source>
+        <translation>
+            <numerusform>%Ln секундтан кейін ұйықтату</numerusform>
+            <numerusform>%Ln секундтан кейін ұйықтату</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="134"/>
+        <source>Hibernating in %Ln second(s)</source>
+        <translation>
+            <numerusform>%Ln секундтан кейін гибернация</numerusform>
+            <numerusform>%Ln секундтан кейін гибернация</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../batterywatcher.cpp" line="137"/>
+        <source>Shutting down in %Ln second(s)</source>
+        <translation>
+            <numerusform>%Ln секундтан кейін сөндіру</numerusform>
+            <numerusform>%Ln секундтан кейін сөндіру</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>LXQt::PowerProfiles</name>
+    <message>
+        <location filename="../PowerProfiles.cpp" line="38"/>
+        <source>power-saver</source>
+        <comment>power-profiles-daemon</comment>
+        <translation>қуатты үнемдеу</translation>
+    </message>
+    <message>
+        <location filename="../PowerProfiles.cpp" line="39"/>
+        <source>balanced</source>
+        <comment>power-profiles-daemon</comment>
+        <translation>теңгерілген</translation>
+    </message>
+    <message>
+        <location filename="../PowerProfiles.cpp" line="40"/>
+        <source>performance</source>
+        <comment>power-profiles-daemon</comment>
+        <translation>өнімділік</translation>
+    </message>
+    <message>
+        <location filename="../PowerProfiles.cpp" line="69"/>
+        <source>Power profile</source>
+        <translation>Қуат профилі</translation>
+    </message>
+</context>
+<context>
+    <name>PowerButton</name>
+    <message>
+        <location filename="../powerbutton.cpp" line="72"/>
+        <source>Power off key action</source>
+        <translation>Қуатты өшіру пернесінің әрекеті</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="79"/>
+        <source>Suspend key action</source>
+        <translation>Күту режимі пернесінің әрекеті</translation>
+    </message>
+    <message>
+        <location filename="../powerbutton.cpp" line="86"/>
+        <source>Hibernate key action</source>
+        <translation>Ұйқы режимі пернесінің әрекеті</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManagementd</name>
+    <message>
+        <location filename="../powermanagementd.cpp" line="115"/>
+        <source>Power Management</source>
+        <translation>Қуатты басқару</translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="116"/>
+        <source>You are running LXQt Power Management for the first time.
+You can configure it from settings... </source>
+        <translation>Сіз LXQt қуатты басқару бағдарламасын бірінші рет іске қосып тұрсыз.
+Оны баптаулардан баптауға болады... </translation>
+    </message>
+    <message>
+        <location filename="../powermanagementd.cpp" line="117"/>
+        <source>Configure...</source>
+        <translation>Баптау...</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../trayicon.cpp" line="70"/>
+        <source>Configure</source>
+        <translation>Баптау</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="78"/>
+        <source>30 minutes</source>
+        <translation>30 минут</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="82"/>
+        <source>1 hour</source>
+        <translation>1 сағат</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="86"/>
+        <source>2 hours</source>
+        <translation>2 сағат</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="90"/>
+        <source>3 hours</source>
+        <translation>3 сағат</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="94"/>
+        <source>4 hours</source>
+        <translation>4 сағат</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="99"/>
+        <source>Pause idleness checks</source>
+        <translation>Әрекетсіздікті тексеруді кідірту</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="107"/>
+        <location filename="../trayicon.cpp" line="199"/>
+        <source>About</source>
+        <translation>Туралы</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="109"/>
+        <source>Disable icon</source>
+        <translation>Таңбашаны сөндіру</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="168"/>
+        <source>Empty</source>
+        <translation>Бос</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="200"/>
+        <source>&lt;p&gt;  &lt;b&gt;LXQt Power Management&lt;/b&gt;&lt;br/&gt;  - Power Management for the LXQt Desktop Environment&lt;/p&gt;&lt;p&gt;  Authors:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - and others from the Razor and LXQt projects&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2025&lt;/p&gt;</source>
+        <translation>&lt;p&gt;  &lt;b&gt;LXQt қуатты басқару&lt;/b&gt;&lt;br/&gt;  - LXQt жұмыс үстелі ортасына арналған қуатты басқару&lt;/p&gt;&lt;p&gt;  Авторлар:&lt;br/&gt;  &amp;nbsp; Christian Surlykke, Alec Moskvin&lt;br/&gt;  &amp;nbsp; - және Razor мен LXQt жобаларының басқалары&lt;/p&gt;&lt;p&gt;  Copyright &amp;copy; 2012-2025&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="218"/>
+        <source>LXQt Power Management info</source>
+        <translation>LXQt қуатты басқару ақпараты</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="219"/>
+        <source>The LXQt Power Management tray icon can be (re)enabled in &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt;</source>
+        <translation>LXQt қуатты басқарудың жүйелік трей таңбашасын &lt;i&gt;lxqt-config-powermanagement&lt;/i&gt; ішінде (қайта) іске қосуға болады</translation>
+    </message>
+    <message>
+        <location filename="../trayicon.cpp" line="221"/>
+        <source>Configure now</source>
+        <translation>Қазір баптау</translation>
+    </message>
+</context>
+</TS>
