a10ddf388544d1750bd896a59f6667af3e39a029
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2026-03-17 09:34:40 +0100

    Weblate commits (#479)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/ru/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement
    
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Moo <hazap@hotmail.com>
    Co-authored-by: milotype <mail@milotype.de>
    Co-authored-by: p-bo <pavel.borecki@gmail.com>

--- config/translations/lxqt-config-powermanagement_cs.ts
+++ config/translations/lxqt-config-powermanagement_cs.ts
@@ -128,7 +128,7 @@
         <location filename="../idlenesswatchersettings.ui" line="80"/>
         <location filename="../idlenesswatchersettings.ui" line="133"/>
         <source>Disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Zakázáno</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="159"/>
@@ -165,24 +165,24 @@
     <message>
         <location filename="../idlenesswatchersettings.ui" line="26"/>
         <source>On AC</source>
-        <translation type="unfinished"></translation>
+        <translation>Při napájení ze sítě</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="38"/>
         <location filename="../idlenesswatchersettings.ui" line="103"/>
         <source>When idle:</source>
-        <translation type="unfinished"></translation>
+        <translation>Při nečinnosti:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="87"/>
         <location filename="../idlenesswatchersettings.ui" line="140"/>
         <source>Turn off monitor(s) after:</source>
-        <translation type="unfinished"></translation>
+        <translation>Vypnout monitor(y) po uplynutí:</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="97"/>
         <source>On Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>Při napájení z akumulátoru</translation>
     </message>
     <message>
         <location filename="../idlenesswatchersettings.ui" line="236"/>
@@ -207,7 +207,7 @@
     <message>
         <location filename="../idlenesswatchersettings.cpp" line="97"/>
         <source>Set to minimum to disable</source>
-        <translation type="unfinished"></translation>
+        <translation>Vypnete nastavením na minimum</translation>
     </message>
 </context>
 <context>
