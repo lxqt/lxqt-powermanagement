ccabfc398f56f0b41b362334ecc9b969cdbd695f
Weblate <noreply@weblate.org>
2026-07-01 09:36:49 +0200

    Weblate commits
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: fhc <hffqyd@sina.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment-battery-info/zh_CN/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-powermanagment/zh_CN/
    Translation: LXQt Configuration/lxqt-config-powermanagement
    Translation: LXQt Configuration/lxqt-powermanagement

--- src/translations/lxqt-powermanagement_zh_CN.ts
+++ src/translations/lxqt-powermanagement_zh_CN.ts
@@ -254,24 +254,24 @@
         <location filename="../PowerProfiles.cpp" line="38"/>
         <source>power-saver</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>电源节能</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="39"/>
         <source>balanced</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>平衡</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="40"/>
         <source>performance</source>
         <comment>power-profiles-daemon</comment>
-        <translation type="unfinished"></translation>
+        <translation>高效</translation>
     </message>
     <message>
         <location filename="../PowerProfiles.cpp" line="69"/>
         <source>Power profile</source>
-        <translation type="unfinished"></translation>
+        <translation>电源设置</translation>
     </message>
 </context>
 <context>
